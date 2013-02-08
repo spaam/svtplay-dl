289cd22b45e4635564f4549ffbf7f7e74641754a
Johan Andersson <j@i19.se>
2013-02-08 12:34:28 +0100

    Support for radioplay.se

--- svtplay_dl.py
+++ svtplay_dl.py
@@ -1078,6 +1078,46 @@ class Ruv(object):
         base_url = m3u8_url.rsplit("/", 1)[0]
         download_hls(options, m3u8_url, base_url)
 
+class Radioplay(object):
+    def handle(self, url):
+        return "radioplay.se" in url
+
+    def get(self, options, url):
+        data = get_http_data(url)
+        match = re.search("liveStationsRedundancy = ({.*});</script>", data)
+        parse = urlparse(url)
+        station = parse.path[1:]
+        streams = None
+        if match:
+            data = json.loads(match.group(1))
+            for i in data["stations"]:
+                if station == i["name"].lower().replace(" ", ""):
+                    streams = i["streams"]
+                    break
+        else:
+            log.error("Can't find any streams.")
+            sys.exit(2)
+        if streams:
+            if options.hls:
+                try:
+                    m3u8_url = streams["hls"]
+                    base_url = m3u8_url.rsplit("/", 1)[0]
+                    download_hls(options, m3u8_url, base_url)
+                except KeyError:
+                    log.error("Can't find any streams.")
+                    sys.error(2)
+            else:
+                try:
+                    rtmp = streams["rtmp"]
+                    download_rtmp(options, rtmp)
+                except KeyError:
+                    mp3 = streams["mp3"]
+                    download_http(options, mp3)
+
+        else:
+            log.error("Can't find any streams.")
+            sys.exit(2)
+
 def progressbar(total, pos, msg=""):
     """
     Given a total and a progress position, output a progress bar
@@ -1113,8 +1153,8 @@ def progressbar(total, pos, msg=""):
     progress_stream.write(fmt % (pos, total, bar, msg))
 
 def get_media(url, options):
-    sites = [Aftonbladet(), Dr(), Expressen(), Hbo(), Justin(), Kanal5(), Kanal9(),
-             Nrk(), Qbrick(), Ruv(), Sr(), Svtplay(), Tv4play(), Urplay(), Viaplay()]
+    sites = [Aftonbladet(), Dr(), Expressen(), Hbo(), Justin(), Kanal5(), Kanal9(), Nrk(),
+            Qbrick(), Ruv(), Radioplay(), Sr(), Svtplay(), Tv4play(), Urplay(), Viaplay()]
     stream = None
     for i in sites:
         if i.handle(url):
