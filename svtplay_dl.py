6773531ec35dd86d7b7f80d4d9ec352008cdb6bd
Johan Andersson <j@i19.se>
2013-02-21 20:16:03 +0100

    kanal5.se is now "closed"

--- svtplay_dl.py
+++ svtplay_dl.py
@@ -905,48 +905,6 @@ class Kanal5():
         if options.subtitle:
             subtitle_json(options, subtitle)
 
-class Kanal9():
-    def handle(self, url):
-        return "kanal5.se" in url
-
-    def get(self, options, url):
-        data = get_http_data(url)
-        match = re.search("@videoPlayer\" value=\"(.*)\"", data)
-        if not match:
-            match = re.search("videoId=(\d+)&player", data)
-            if not match:
-                log.error("Can't find video file")
-                sys.exit(2)
-        try:
-            from pyamf import remoting
-        except ImportError:
-            log.error("You need to install pyamf to download content from kanal5.se and kanal9play")
-            log.error("In debian the package is called python-pyamf")
-            sys.exit(2)
-
-        player_id = 811317479001
-        publisher_id = 22710239001
-        const = "9f79dd85c3703b8674de883265d8c9e606360c2e"
-        env = remoting.Envelope(amfVersion=3)
-        env.bodies.append(("/1", remoting.Request(target="com.brightcove.player.runtime.PlayerMediaFacade.findMediaById", body=[const, player_id, match.group(1), publisher_id], envelope=env)))
-        env = str(remoting.encode(env).read())
-        url = "http://c.brightcove.com/services/messagebroker/amf?playerKey=AQ~~,AAAABUmivxk~,SnCsFJuhbr0vfwrPJJSL03znlhz-e9bk"
-        header = "application/x-amf"
-        data = get_http_data(url, "POST", header, env)
-        streams = {}
-
-        for i in remoting.decode(data).bodies[0][1].body['renditions']:
-            stream = {}
-            stream["uri"] = i["defaultURL"]
-            streams[i["encodingRate"]] = stream
-
-        test = select_quality(options, streams)
-
-        filename = test["uri"]
-        match = re.search("(rtmp[e]{0,1}://.*)\&(.*)$", filename)
-        options.other = "-W %s -y %s " % ("http://admin.brightcove.com/viewer/us1.25.04.01.2011-05-24182704/connection/ExternalConnection_2.swf", match.group(2))
-        download_rtmp(options, match.group(1))
-
 class Expressen():
     def handle(self, url):
         return "expressen.se" in url
@@ -1322,7 +1280,7 @@ def progressbar(total, pos, msg=""):
     progress_stream.write(fmt % (pos, total, bar, msg))
 
 def get_media(url, options):
-    sites = [Aftonbladet(), Dr(), Expressen(), Hbo(), Justin(), Kanal5(), Kanal9(), Nrk(),
+    sites = [Aftonbladet(), Dr(), Expressen(), Hbo(), Justin(), Kanal5(), Nrk(),
             Qbrick(), Ruv(), Radioplay(), Sr(), Svtplay(), Tv4play(), Urplay(), Viaplay()]
     stream = None
     for i in sites:
