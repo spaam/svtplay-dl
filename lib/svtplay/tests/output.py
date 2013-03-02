663287b541056db45d172fc2ac04c6293299cf76
Olof Johansson <olof@ethup.se>
2013-03-02 21:26:28 +0100

    Add editor modelines

--- lib/svtplay/tests/output.py
+++ lib/svtplay/tests/output.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# ex:ts=4:sw=4:sts=4:et
+# -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
 from __future__ import absolute_import
 import unittest
 import svtplay.output
