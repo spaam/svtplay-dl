a6ec593c0a981819b43fc267bbb511b11a4d7b41
Olof Johansson <olof@ethup.se>
2013-03-02 22:01:25 +0100

    tests: Silence pylint naming conventions warning
    The naming conventions of the unittest module does not adhere to
    Python coding standards as specified in PEP8. This isn't our fault
    however...

--- lib/svtplay/tests/output.py
+++ lib/svtplay/tests/output.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 # ex:ts=4:sw=4:sts=4:et
 # -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+# The unittest framwork doesn't play nice with pylint:
+#   pylint: disable-msg=C0103
+
 from __future__ import absolute_import
 import unittest
 import svtplay.output
