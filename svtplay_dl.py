4cca843143bab51c7e3bfc5d748b2760a48f182d
Johan Andersson <j@i19.se>
2013-03-05 23:35:00 +0100

    download_http: adding a better U-A. need this for vimeo.

--- svtplay_dl.py
+++ svtplay_dl.py
@@ -474,7 +474,14 @@ def download_hls(options, url, baseurl=None):
 
 def download_http(options, url):
     """ Get the stream from HTTP """
-    response = urlopen(url)
+    request = Request(url)
+    request.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+    try:
+        response = urlopen(request)
+    except HTTPError as e:
+        print "ERROR"
+        print e.reason
+        sys.exit()
     try:
         total_size = response.info()['Content-Length']
     except KeyError:
