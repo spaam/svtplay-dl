15c4f99f7d1ef3a9b4a14ac68e466584b5922319
Johan Andersson <j@i19.se>
2013-03-09 13:59:49 +0100

    kanal5play: support for user

--- svtplay_dl.py
+++ svtplay_dl.py
@@ -897,15 +897,42 @@ class Kanal5():
         return ("kanal5play.se" in url) or ("kanal9play.se" in url)
 
     def get(self, options, url):
+        cj = CookieJar()
         match = re.search(".*video/([0-9]+)", url)
         if not match:
             log.error("Can't find video file")
             sys.exit(2)
+        video_id = match.group(1)
+        if options.username and options.password:
+            #bogus
+            cc = Cookie(None, 'asdf', None, '80', '80', 'www.kanal5play.se', None, None, '/', None, False, False, 'TestCookie', None, None, None)
+            cj.set_cookie(cc)
+            #get session cookie
+            data = get_http_data("http://www.kanal5play.se/", cookiejar=cj)
+            authurl = "https://kanal5swe.appspot.com/api/user/login?callback=jQuery171029989&email=%s&password=%s&_=136250" % (options.username, options.password)
+            data = get_http_data(authurl)
+            match = re.search("({.*})\);", data)
+            jsondata = json.loads(match.group(1))
+            if jsondata["success"] == False:
+                log.error(jsondata["message"])
+                sys.exit(2)
+            authToken = jsondata["userData"]["auth"]
+            cc = Cookie(version=0, name='authToken',
+                          value=authToken,
+                          port=None, port_specified=False,
+                          domain='www.kanal5play.se',
+                          domain_specified=True,
+                          domain_initial_dot=True, path='/',
+                          path_specified=True, secure=False,
+                          expires=None, discard=True, comment=None,
+                          comment_url=None, rest={'HttpOnly': None})
+            cj.set_cookie(cc)
+
         format = "FLASH"
         if options.hls:
             format = "IPHONE"
-        url = "http://www.kanal5play.se/api/getVideo?format=%s&videoId=%s" % (format, match.group(1))
-        data = json.loads(get_http_data(url))
+        url = "http://www.kanal5play.se/api/getVideo?format=%s&videoId=%s" % (format, video_id)
+        data = json.loads(get_http_data(url, cookiejar=cj))
         options.live = data["isLive"]
         if data["hasSubtitle"]:
             subtitle = "http://www.kanal5play.se/api/subtitles/%s" % match.group(1)
@@ -933,7 +960,7 @@ class Kanal5():
             download_rtmp(options, steambaseurl)
         if options.subtitle:
             if options.output != "-":
-                data = get_http_data(data)
+                data = get_http_data(data, cookiejar=cj)
                 subtitle_json(options, data)
 
 class Expressen():
