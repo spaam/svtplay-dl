329952410d9d4a225aab0aa752dd054ec5383a89
Johan Andersson <j@i19.se>
2013-03-23 15:17:15 +0100

    Use __version__ from the lib

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 import sys
+import os
+
+srcdir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib/")
+sys.path.insert(0, srcdir)
+import svtplay_dl
 
 setup(
     name = "svtplay-dl",
-    version = "0.9.2013.03.06",    # FIXME - extract from svtplay-dl
+    version = svtplay_dl.__version__,
     packages = find_packages(
         'lib',
         exclude=["tests", "*.tests", "*.tests.*"]),
     package_dir = {'': 'lib'},
-    scripts = ['svtplay-dl'],
+    scripts = ['bin/svtplay-dl'],
 
     author = "Johan Andersson",
     author_email = "j@i19.se",
