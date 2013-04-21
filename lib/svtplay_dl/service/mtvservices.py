078df0b3e5cba80a1b18ca0d2e2b86b055d23f65
Johan Andersson <j@i19.se>
2013-04-21 21:59:25 +0200

    forgot to add mtvservices.py

---
+++ lib/svtplay_dl/service/mtvservices.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import
+import sys
+import re
+import json
+import xml.etree.ElementTree as ET
+
+from svtplay_dl.utils import get_http_data
+from svtplay_dl.service import Service
+from svtplay_dl.utils import get_http_data, select_quality
+from svtplay_dl.fetcher.http import download_http
+
+from svtplay_dl.log import log
+
+class Mtvservices(Service):
+    def handle(self, url):
+        return ("colbertnation.com" in url) or ("www.thedailyshow.com" in url)
+
+    def get(self, options, url):
+        data = get_http_data(url)
+        match = re.search("mgid=\"(mgid.*[0-9]+)\" data-wi", data)
+        if not match:
+       		sys.exit(2)
+       	url = "http://media.mtvnservices.com/player/html5/mediagen/?uri=%s" % match.group(1)
+       	data = get_http_data(url)
+       	start = data.index("<?xml version=")
+       	data = data[start:]
+       	xml = ET.XML(data)
+       	dada = xml.find("package")
+       	ss = xml.find("video").find("item")
+        if sys.version_info < (2, 7):
+            sa = list(ss.getiterator("rendition"))
+        else:
+            sa = list(ss.iter("rendition"))
+        streams = {}
+        for i in sa:
+        	streams[int(i.attrib["height"])] = i.find("src").text
+        stream = select_quality(options, streams)
+        temp = stream.index("gsp.comedystor")
+        url = "http://mtvnmobile.vo.llnwd.net/kip0/_pxn=0+_pxK=18639+_pxE=mp4/44620/mtvnorigin/%s" % stream[temp:]
+        download_http(options, url)
+
+
