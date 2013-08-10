feb480a8f13b5d106779b5f8b5935caf1c2b5266
Johan Andersson <j@i19.se>
2013-08-10 21:21:28 +0200

    New version

--- lib/svtplay_dl/__init__.py
+++ lib/svtplay_dl/__init__.py
@@ -12,7 +12,7 @@ from svtplay_dl.utils import get_http_data
 from svtplay_dl.service import service_handler, Generic
 
 
-__version__ = "0.9.2013.05.06"
+__version__ = "0.9.2013.08.10"
 
 class Options:
     """
