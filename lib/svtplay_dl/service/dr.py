4d4e785dd570d06fbfb47f77583061c80f5d2c06
Johan Andersson <j@i19.se>
2013-09-14 22:39:37 +0200

    dr.dk: News still use the old thing

--- lib/svtplay_dl/service/dr.py
+++ lib/svtplay_dl/service/dr.py
@@ -16,39 +16,58 @@ class Dr(Service):
     def get(self, options, url):
         data = get_http_data(url)
         match = re.search(r'resource:[ ]*"([^"]*)",', data)
-        resource_url = match.group(1)
-        resource_data = get_http_data(resource_url)
-        resource = json.loads(resource_data)
-        tempresource = resource['Data'][0]['Assets']
-        # To find the VideoResource, they have Images as well
-        for resources in tempresource:
-            if resources['Kind'] == 'VideoResource':
-                links = resources['Links']
-                break
+        if match:
+            resource_url = match.group(1)
+            resource_data = get_http_data(resource_url)
+            resource = json.loads(resource_data)
+            tempresource = resource['Data'][0]['Assets']
+            # To find the VideoResource, they have Images as well
+            for resources in tempresource:
+                if resources['Kind'] == 'VideoResource':
+                    links = resources['Links']
+                    break
 
-        streams = {}
-        for i in links:
-            if options.hls:
-                if i["Target"] == "Ios":
-                    stream = {}
-                    stream["uri"] = i["Uri"]
-                    streams[int(i["Bitrate"])] = stream
+            streams = {}
+            for i in links:
+                if options.hls:
+                    if i["Target"] == "Ios":
+                        stream = {}
+                        stream["uri"] = i["Uri"]
+                        streams[int(i["Bitrate"])] = stream
+                else:
+                    if i["Target"] == "Streaming":
+                        stream = {}
+                        stream["uri"] = i["Uri"]
+                        streams[int(i["Bitrate"])] = stream
+
+            if len(streams) == 1:
+                test = streams[list(streams.keys())[0]]
             else:
-                if i["Target"] == "Streaming":
-                    stream = {}
-                    stream["uri"] = i["Uri"]
-                    streams[int(i["Bitrate"])] = stream
+                test = select_quality(options, streams)
 
-        if len(streams) == 1:
-            test = streams[list(streams.keys())[0]]
+            if options.hls:
+                baseurl = test["uri"][0:test["uri"].rfind("/")]
+                download_hls(options, test["uri"], baseurl=baseurl)
+            else:
+                options.other = "-y '%s'" % test["uri"].replace("rtmp://vod.dr.dk/cms/", "")
+                rtmp = "rtmp://vod.dr.dk/cms/"
+                download_rtmp(options, rtmp)
         else:
-            test = select_quality(options, streams)
+            match = re.search(r'resource="([^"]*)"', data)
+            if not match:
+                log.error("Cant find resource info for this video")
+                sys.exit(2)
+            resource_url = "http://www.dr.dk%s" % match.group(1)
+            resource_data = get_http_data(resource_url)
+            resource = json.loads(resource_data)
+            streams = {}
+            for stream in resource['links']:
+                streams[stream['bitrateKbps']] = stream['uri']
+            if len(streams) == 1:
+                uri = streams[list(streams.keys())[0]]
+            else:
+                uri = select_quality(options, streams)
 
-        if options.hls:
-            baseurl = test["uri"][0:test["uri"].rfind("/")]
-            download_hls(options, test["uri"], baseurl=baseurl)
-        else:
-            options.other = "-y '%s'" % test["uri"].replace("rtmp://vod.dr.dk/cms/", "")
+            options.other = "-v -y '" + uri.replace("rtmp://vod.dr.dk/cms/", "") + "'"
             rtmp = "rtmp://vod.dr.dk/cms/"
             download_rtmp(options, rtmp)
-
