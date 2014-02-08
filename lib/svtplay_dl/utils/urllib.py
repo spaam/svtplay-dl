e8dc4781dbe4567369b89d55c27c8d63ac59b6cd
Olof Johansson <olof@ethup.se>
2014-02-08 16:41:55 +0100

    urllib: export quote (url encoding)

--- lib/svtplay_dl/utils/urllib.py
+++ lib/svtplay_dl/utils/urllib.py
@@ -9,14 +9,14 @@ from __future__ import absolute_import
 from svtplay_dl.utils import is_py3
 if is_py3:
     # pylint: disable=E0611
-    from urllib.parse import unquote_plus, quote_plus, urlparse, parse_qs
+    from urllib.parse import quote, unquote_plus, quote_plus, urlparse, parse_qs
     from urllib.request import urlopen, Request, build_opener, \
                                HTTPCookieProcessor, HTTPRedirectHandler
     from urllib.error import HTTPError, URLError
     from urllib.response import addinfourl
     from http.cookiejar import CookieJar, Cookie
 else:
-    from urllib import addinfourl, unquote_plus, quote_plus
+    from urllib import addinfourl, quote, unquote_plus, quote_plus
     from urlparse import urlparse, parse_qs
     from urllib2 import urlopen, Request, HTTPError, URLError, build_opener, \
                         HTTPCookieProcessor, HTTPRedirectHandler
