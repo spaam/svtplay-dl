0a5f2834756b7e91137d6a1df21cfd6c531ff226
Johan Andersson <j@i19.se>
2014-04-02 20:14:38 +0200

    Forgot to remove tab in __main__

--- lib/svtplay_dl/__main__.py
+++ lib/svtplay_dl/__main__.py
@@ -8,7 +8,6 @@ if __package__ is None and not hasattr(sys, "frozen"):
     import os.path
     sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-    import svtplay_dl
-
-    if __name__ == '__main__':
-        svtplay_dl.main()
+import svtplay_dl
+if __name__ == '__main__':
+    svtplay_dl.main()
