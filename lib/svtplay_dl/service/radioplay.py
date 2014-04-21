153615e918a928a19e45ec751b90500f3523cfa4
Johan Andersson <j@i19.se>
2014-04-21 19:05:06 +0200

    radioplay: convert into new video fetcher

--- lib/svtplay_dl/service/radioplay.py
+++ lib/svtplay_dl/service/radioplay.py
@@ -11,9 +11,9 @@ import json
 from svtplay_dl.utils.urllib import urlparse
 from svtplay_dl.service import Service
 
-from svtplay_dl.fetcher.rtmp import download_rtmp
-from svtplay_dl.fetcher.hls import download_hls
-from svtplay_dl.fetcher.http import download_http
+from svtplay_dl.fetcher.rtmp import RTMP
+from svtplay_dl.fetcher.hls import HLS
+from svtplay_dl.fetcher.http import HTTP
 
 from svtplay_dl.log import log
 
@@ -21,35 +21,11 @@ class Radioplay(Service):
     supported_domains = ['radioplay.se']
 
     def get(self, options):
-        match = re.search(r"liveStationsRedundancy = ({.*});</script>", self.get_urldata())
-        parse = urlparse(self.url)
-        station = parse.path[1:]
-        streams = None
+        match = re.search(r"RP.vcdData = ({.*});</script>", self.get_urldata())
         if match:
             data = json.loads(match.group(1))
-            for i in data["stations"]:
-                if station == i["name"].lower().replace(" ", ""):
-                    streams = i["streams"]
-                    break
+            for i in list(data["station"]["streams"].keys()):
+                yield HTTP(options, data["station"]["streams"][i], i)
         else:
-            log.error("Can't find any streams.")
-            sys.exit(2)
-        if streams:
-            if options.hls:
-                try:
-                    m3u8_url = streams["hls"]
-                    download_hls(options, m3u8_url)
-                except KeyError:
-                    log.error("Can't find any streams.")
-                    sys.exit(2)
-            else:
-                try:
-                    rtmp = streams["rtmp"]
-                    download_rtmp(options, rtmp)
-                except KeyError:
-                    mp3 = streams["mp3"]
-                    download_http(options, mp3)
-
-        else:
-            log.error("Can't find any streams.")
+            log.error("Can't find stream info")
             sys.exit(2)
