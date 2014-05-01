86576128d5e302c3d1b9772f3566a2b92b3571e3
Olof Johansson <olof@ethup.se>
2014-05-01 22:44:38 +0200

    Add initial unittests for expressen

---
+++ lib/svtplay_dl/service/tests/expressen.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# ex:ts=4:sw=4:sts=4:et
+# -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+# The unittest framwork doesn't play nice with pylint:
+#   pylint: disable-msg=C0103
+
+from __future__ import absolute_import
+import unittest
+from svtplay_dl.service.tests import HandlesURLsTestMixin
+from svtplay_dl.service.expressen import Expressen
+
+class handlesTest(unittest.TestCase, HandlesURLsTestMixin):
+    service = Expressen
+    urls = {
+        "ok": [
+            "http://www.expressen.se/tv/nyheter/kungligt/se-nar-estelle-stjal-kungens-show/",
+        ],
+        "bad": [
+            "http://www.oppetarkiv.se/video/1129844/jacobs-stege-ep1",
+            "http://www.dn.se/nyheter/sverige/det-ar-en-dodsfalla"
+        ]
+    }
