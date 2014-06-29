9fe5f1792b39678e7d9a90b39c250e09560a81a3
Johan Andersson <j@i19.se>
2014-06-29 23:12:56 +0200

    ruv: update the service

--- lib/svtplay_dl/service/ruv.py
+++ lib/svtplay_dl/service/ruv.py
@@ -3,8 +3,11 @@
 from __future__ import absolute_import
 import re
 import copy
+import sys
+import json
 
 from svtplay_dl.service import Service
+from svtplay_dl.log import log
 from svtplay_dl.utils import get_http_data
 from svtplay_dl.fetcher.hls import HLS, hlsparse
 
@@ -12,14 +15,24 @@ class Ruv(Service):
     supported_domains = ['ruv.is']
 
     def get(self, options):
-        data = self.get_urldata()
-        match = re.search(r'(http://load.cache.is/vodruv.*)"', data)
-        js_url = match.group(1)
-        js = get_http_data(js_url)
-        tengipunktur = js.split('"')[1]
-        match = re.search(r"http.*tengipunktur [+] '([:]1935.*)'", data)
-        m3u8_url = "http://" + tengipunktur + match.group(1)
-        streams = hlsparse(m3u8_url)
-        for n in list(streams.keys()):
-            yield HLS(copy.copy(options), streams[n], n)
+        match = re.search(r'"([^"]+geo.php)"', self.get_urldata())
+        if match:
+            data = get_http_data(match.group(1))
+            match = re.search(r'punktur=\(([^ ]+)\)', data)
+            if match:
+                janson = json.loads(match.group(1))
+                options.live = checklive(janson["result"][1])
+                streams = hlsparse(janson["result"][1])
+                for n in list(streams.keys()):
+                    yield HLS(copy.copy(options), streams[n], n)
+        else:
+            match = re.search(r'<source [^ ]*[ ]*src="([^"]+)" ', self.get_urldata())
+            if not match:
+                log.error("Can't find video info")
+                sys.exit(2)
+            m3u8_url = match.group(1)
+            options.live = checklive(m3u8_url)
+            yield HLS(copy.copy(options), m3u8_url, 800)
 
+def checklive(url):
+    return True if re.search("live", url) else False
