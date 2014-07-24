17500539a5f0ab7772e43295c0085f540ae98c64
Olof Johansson <olof@ethup.se>
2014-07-24 18:39:15 +0200

    Fix unittest failures for progessbar
    This solution is not super nice, but solves the current errors. A proper mock
    based solution would probably be preferable.
    
    Reference: #ep14boat

--- lib/svtplay_dl/tests/output.py
+++ lib/svtplay_dl/tests/output.py
@@ -38,49 +38,49 @@ class progressTest(unittest.TestCase):
 
 class progressbarTest(unittest.TestCase):
     def setUp(self):
+        self.old_termsiz = svtplay_dl.output.get_terminal_size
+        svtplay_dl.output.get_terminal_size = lambda: (50, 25)
+
         self.mockfile = mockfile()
         svtplay_dl.output.progress_stream = self.mockfile
+
+    def tearDown(self):
+        svtplay_dl.output.get_terminal_size = self.old_termsiz
 
     def test_0_100(self):
         svtplay_dl.output.progressbar(100, 0)
         self.assertEqual(
-            self.mockfile.read(),
-            "\r[000/100][..................................................] "
+            self.mockfile.read(), "\r[000/100][.........................] "
         )
 
     def test_progress_1_100(self):
         svtplay_dl.output.progressbar(100, 1)
         self.assertEqual(
-            self.mockfile.read(),
-            "\r[001/100][..................................................] "
+            self.mockfile.read(), "\r[001/100][.........................] "
         )
 
     def test_progress_2_100(self):
         svtplay_dl.output.progressbar(100, 2)
         self.assertEqual(
-            self.mockfile.read(),
-            "\r[002/100][=.................................................] "
+            self.mockfile.read(), "\r[002/100][.........................] "
         )
 
     def test_progress_50_100(self):
         svtplay_dl.output.progressbar(100, 50)
         self.assertEqual(
-            self.mockfile.read(),
-            "\r[050/100][=========================.........................] "
+            self.mockfile.read(), "\r[050/100][============.............] "
         )
 
     def test_progress_100_100(self):
         svtplay_dl.output.progressbar(100, 100)
         self.assertEqual(
-            self.mockfile.read(),
-            "\r[100/100][==================================================] "
+            self.mockfile.read(), "\r[100/100][=========================] "
         )
 
     def test_progress_20_100_msg(self):
         svtplay_dl.output.progressbar(100, 20, "msg")
         self.assertEqual(
-            self.mockfile.read(),
-            "\r[020/100][==========........................................] msg"
+            self.mockfile.read(), "\r[020/100][=====....................] msg"
         )
 
 class EtaTest(unittest.TestCase):
