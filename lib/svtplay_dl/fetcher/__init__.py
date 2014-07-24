5e8f36305a20946988023278d88d53957ee9a2d1
Olof Johansson <olof@ethup.se>
2014-07-24 20:19:22 +0200

    VideoRetriever: default to bitrate=0
    Reference: #ep14boat

--- lib/svtplay_dl/fetcher/__init__.py
+++ lib/svtplay_dl/fetcher/__init__.py
@@ -1,5 +1,5 @@
 class VideoRetriever:
-    def __init__(self, options, url, bitrate, **kwargs):
+    def __init__(self, options, url, bitrate=0, **kwargs):
         self.options = options
         self.url = url
         self.bitrate = int(bitrate)
