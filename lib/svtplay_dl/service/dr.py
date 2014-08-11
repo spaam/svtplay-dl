9b6f838e16d4b0054dcce1d304dd987402260ad7
Johan Andersson <j@i19.se>
2014-08-11 19:46:56 +0200

    dr.dk: fixed a crash when looking on news videos

--- lib/svtplay_dl/service/dr.py
+++ lib/svtplay_dl/service/dr.py
@@ -23,24 +23,9 @@ class Dr(Service, OpenGraphThumbMixin):
             resource_url = match.group(1)
             resource_data = get_http_data(resource_url)
             resource = json.loads(resource_data)
-            tempresource = resource['Data'][0]['Assets']
-            # To find the VideoResource, they have Images as well
-            for resources in tempresource:
-                if resources['Kind'] == 'VideoResource':
-                    links = resources['Links']
-                    break
-
-            for i in links:
-                if i["Target"] == "Ios":
-                    streams = hlsparse(i["Uri"])
-                    for n in list(streams.keys()):
-                        yield HLS(copy.copy(options), streams[n], n)
-                else:
-                    if i["Target"] == "Streaming":
-                        options.other = "-y '%s'" % i["Uri"].replace("rtmp://vod.dr.dk/cms/", "")
-                        rtmp = "rtmp://vod.dr.dk/cms/"
-                        yield RTMP(copy.copy(options), rtmp, i["Bitrate"])
-
+            streams = find_stream(options, resource)
+            for i in streams:
+                yield i
         else:
             match = re.search(r'resource="([^"]*)"', data)
             if not match:
@@ -50,17 +35,40 @@ class Dr(Service, OpenGraphThumbMixin):
             resource_data = get_http_data(resource_url)
             resource = json.loads(resource_data)
 
-            for stream in resource['Links']:
-                if stream["Target"] == "HDS":
-                    manifest = "%s?hdcore=2.8.0&g=hejsan" % stream["Uri"]
-                    streams = hdsparse(copy.copy(options), manifest)
-                    for n in list(streams.keys()):
-                        yield streams[n]
-                if stream["Target"] == "HLS":
-                    streams = hlsparse(stream["Uri"])
-                    for n in list(streams.keys()):
-                        yield HLS(copy.copy(options), streams[n], n)
-                if stream["Target"] == "Streaming":
-                    options.other = "-v -y '%s'" % stream['Uri'].replace("rtmp://vod.dr.dk/cms/", "")
-                    rtmp = "rtmp://vod.dr.dk/cms/"
-                    yield RTMP(copy.copy(options), rtmp, stream['Bitrate'])
+            if "Data" in resource:
+                streams = find_stream(options, resource)
+                for i in streams:
+                    yield i
+            else:
+                for stream in resource['Links']:
+                    if stream["Target"] == "HDS":
+                        manifest = "%s?hdcore=2.8.0&g=hejsan" % stream["Uri"]
+                        streams = hdsparse(copy.copy(options), manifest)
+                        for n in list(streams.keys()):
+                            yield streams[n]
+                    if stream["Target"] == "HLS":
+                        streams = hlsparse(stream["Uri"])
+                        for n in list(streams.keys()):
+                            yield HLS(copy.copy(options), streams[n], n)
+                    if stream["Target"] == "Streaming":
+                        options.other = "-v -y '%s'" % stream['Uri'].replace("rtmp://vod.dr.dk/cms/", "")
+                        rtmp = "rtmp://vod.dr.dk/cms/"
+                        yield RTMP(copy.copy(options), rtmp, stream['Bitrate'])
+
+def find_stream(options, resource):
+    tempresource = resource['Data'][0]['Assets']
+    # To find the VideoResource, they have Images as well
+    for resources in tempresource:
+        if resources['Kind'] == 'VideoResource':
+            links = resources['Links']
+            break
+    for i in links:
+        if i["Target"] == "Ios" or i["Target"] == "HLS":
+            streams = hlsparse(i["Uri"])
+            for n in list(streams.keys()):
+                yield HLS(copy.copy(options), streams[n], n)
+        else:
+            if i["Target"] == "Streaming":
+                options.other = "-y '%s'" % i["Uri"].replace("rtmp://vod.dr.dk/cms/", "")
+                rtmp = "rtmp://vod.dr.dk/cms/"
+                yield RTMP(copy.copy(options), rtmp, i["Bitrate"])
