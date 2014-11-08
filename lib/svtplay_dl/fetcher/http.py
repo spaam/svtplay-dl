c9ba7887b0df1648fb8f949e757959551f90c8fb
Johan Andersson <j@i19.se>
2014-11-08 23:02:40 +0100

    HTTP-fetcher: use output filename instead of url

--- lib/svtplay_dl/fetcher/http.py
+++ lib/svtplay_dl/fetcher/http.py
@@ -30,7 +30,7 @@ class HTTP(VideoRetriever):
         total_size = int(total_size)
         bytes_so_far = 0
 
-        file_d = output(self.options, self.url, "mp4")
+        file_d = output(self.options, self.options.output, "mp4")
         if hasattr(file_d, "read") is False:
             return
 
