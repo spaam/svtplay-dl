ab3283aacea466e30f762d17fae108db993b9786
Johan Andersson <j@i19.se>
2015-01-16 21:58:42 +0100

    output: extract the filename and add srt file extension to it.
    Issue is that when we specify filename with -o the subtitles
    is written to that file and later when we want to save the video 
    to that file. the file already exists.

--- lib/svtplay_dl/output.py
+++ lib/svtplay_dl/output.py
@@ -123,6 +123,8 @@ def output(options, extention="mp4", openfd=True):
         ext = re.search(r"(\.[a-z0-9]+)$", options.output)
         if not ext:
             options.output = "%s.%s" % (options.output, extention)
+        if extention == "srt" and ext:
+            options.output = "%s.srt" % options.output[:options.output.rfind(ext.group(1))]
         log.info("Outfile: %s", options.output)
         if (os.path.isfile(options.output) or \
             findexpisode(os.path.dirname(os.path.realpath(options.output)), options.service, os.path.basename(options.output))):
