68854fc53ab87779e7e6be1b095ca6b9c0a81f7e
Johan Andersson <j@i19.se>
2015-01-28 20:50:10 +0100

    get_media: they to the same thing so better have fewer lines

--- lib/svtplay_dl/__init__.py
+++ lib/svtplay_dl/__init__.py
@@ -134,9 +134,7 @@ def get_media(url, options):
             log.error("Cant find that page: %s", e.reason)
             return
     if not stream:
-        if url.find(".f4m") > 0:
-            stream = Raw(url)
-        if url.find(".m3u8") > 0:
+        if url.find(".f4m") > 0 or url.find(".m3u8") > 0:
             stream = Raw(url)
         if not stream:
             log.error("That site is not supported. Make a ticket or send a message")
