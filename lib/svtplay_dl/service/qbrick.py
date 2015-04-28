acb3349591aa932f3dce5b43bc9e19a2f1a73c67
Johan Andersson <j@i19.se>
2015-04-28 23:20:34 +0200

    qbrick: unused import unquote_plus

--- lib/svtplay_dl/service/qbrick.py
+++ lib/svtplay_dl/service/qbrick.py
@@ -7,7 +7,6 @@ import xml.etree.ElementTree as ET
 
 from svtplay_dl.service import Service, OpenGraphThumbMixin
 from svtplay_dl.utils import get_http_data, is_py2_old
-from svtplay_dl.utils.urllib import unquote_plus
 from svtplay_dl.log import log
 from svtplay_dl.fetcher.rtmp import RTMP
 
