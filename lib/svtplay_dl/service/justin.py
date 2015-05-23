e1100fa7d49f1db30bafc06e51495e2b529af02f
Johan Andersson <j@i19.se>
2015-05-23 19:18:04 +0200

    justin: support for the new vod system

--- lib/svtplay_dl/service/justin.py
+++ lib/svtplay_dl/service/justin.py
@@ -8,17 +8,17 @@ from __future__ import absolute_import
 import re
 import json
 import copy
-import xml.etree.ElementTree as ET
 
-from svtplay_dl.utils.urllib import urlparse, quote
+from svtplay_dl.utils.urllib import urlparse, quote_plus
 from svtplay_dl.service import Service
 from svtplay_dl.utils import get_http_data
 from svtplay_dl.log import log
 from svtplay_dl.fetcher.hls import HLS, hlsparse
-from svtplay_dl.fetcher.http import HTTP
+
 
 class JustinException(Exception):
     pass
+
 
 class JustinUrlException(JustinException):
     """
@@ -40,7 +40,6 @@ class Justin(Service):
         r'^(?:(?:[a-z]{2}-)?[a-z]{2}\.)?(www\.)?twitch\.tv$',
         r'^(?:(?:[a-z]{2}-)?[a-z]{2}\.)?(www\.)?justin\.tv$']
 
-    # TODO: verify that this will support Justin as well
     api_base_url = 'https://api.twitch.tv'
     hls_base_url = 'http://usher.justin.tv/api/channel/hls'
 
@@ -51,51 +50,51 @@ class Justin(Service):
         if self.exclude(options):
             return
 
-        for jtv_video_type in [self._get_chapter, self._get_archive,
-                               self._get_channel]:
+        match = re.match(r'/(\w+)/([bcv])/(\d+)', urlp.path)
+        if not match:
             try:
-                jtv_video_type(urlp, options)
-                success = True
-                break
+                for i in self._get_channel(options, urlp):
+                    yield i
             except JustinUrlException as e:
                 log.debug(str(e))
+                log.error("This twitch/justin video type is unsupported")
+                return
+        else:
+            if match.group(2) in ["b", "c"]:
+                log.error("This twitch video type is unsupported")
+                return
+            try:
+                for i in self._get_archive(options, match.group(3)):
+                    yield i
+            except JustinUrlException as e:
+                log.debug(str(e))
+                log.error("This twitch video type is unsupported")
+                return
 
-        if not success:
-            log.debug(str(e))
-            log.error("This twitch/justin video type is unsupported")
-            return
+    def _get_static_video(self, options, videoid):
+        access = self._get_access_token(videoid)
 
+        if "token" not in access:
+            raise JustinUrlException('video', self.url)
+        nauth = quote_plus(str(access["token"]))
+        authsig = access["sig"]
 
-    def _get_static_video(self, vid, options, vidtype):
-        url = "http://api.justin.tv/api/broadcast/by_%s/%s.xml?onsite=true" % (
-            vidtype, vid)
-        error, data = get_http_data(url)
-        if error:
-            log.error("Cant get api page")
-            return
+        url = "http://usher.twitch.tv/vod/%s?nauth=%s&nauthsig=%s" % (
+            videoid, nauth, authsig)
 
-        xml = ET.XML(data)
-        url = xml.find("archive").find("video_file_url").text
+        streams = hlsparse(url)
+        if streams:
+            for n in list(streams.keys()):
+                yield HLS(copy.copy(options), streams[n], n)
 
-        yield HTTP(copy.copy(options), url)
+    def _get_archive(self, options, vid):
+        try:
+            for n in self._get_static_video(options, vid):
+                yield n
+        except JustinUrlException as e:
+            log.error(str(e))
 
-    def _get_archive(self, urlp, options):
-        match = re.match(r'/\w+/b/(\d+)', urlp.path)
-        if not match:
-            raise JustinUrlException('video', urlp.geturl())
-
-        self._get_static_video(match.group(1), options, 'archive')
-
-
-    def _get_chapter(self, urlp, options):
-        match = re.match(r'/\w+/c/(\d+)', urlp.path)
-        if not match:
-            raise JustinUrlException('video', urlp.geturl())
-
-        self._get_static_video(match.group(1), options, 'chapter')
-
-
-    def _get_access_token(self, channel):
+    def _get_access_token(self, channel, vtype="vods"):
         """
         Get a Twitch access token. It's a three element dict:
 
@@ -110,8 +109,7 @@ class Justin(Service):
         Both `sig` and `token` should be added to the HLS URI, and the
         token should, of course, be URI encoded.
         """
-        return self._ajax_get('/api/channels/%s/access_token' % channel)
-
+        return self._ajax_get('/api/%s/%s/access_token' % (vtype, channel))
 
     def _ajax_get(self, method):
         url = "%s/%s" % (self.api_base_url, method)
@@ -129,15 +127,13 @@ class Justin(Service):
         })
         return json.loads(payload)
 
-
     def _get_hls_url(self, channel):
-        access = self._get_access_token(channel)
+        access = self._get_access_token(channel, "channels")
 
-        query = "token=%s&sig=%s" % (quote(access['token']), access['sig'])
+        query = "token=%s&sig=%s" % (quote_plus(access['token']), access['sig'])
         return "%s/%s.m3u8?%s" % (self.hls_base_url, channel, query)
 
-
-    def _get_channel(self, urlp, options):
+    def _get_channel(self, options, urlp):
         match = re.match(r'/(\w+)', urlp.path)
 
         if not match:
