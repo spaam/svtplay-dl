ea70ec30bf3e4dd8fd0a664e35a290d2600907f3
Johan Andersson <j@i19.se>
2015-08-31 20:35:05 +0200

    urllib: remove unused imports

--- lib/svtplay_dl/utils/urllib.py
+++ lib/svtplay_dl/utils/urllib.py
@@ -10,14 +10,6 @@ from svtplay_dl.utils import is_py3
 if is_py3:
     # pylint: disable=E0611
     from urllib.parse import quote, unquote_plus, quote_plus, urlparse, parse_qs, urljoin
-    from urllib.request import urlopen, Request, build_opener, \
-                               HTTPCookieProcessor, HTTPRedirectHandler
-    from urllib.error import HTTPError, URLError
-    from urllib.response import addinfourl
-    from http.cookiejar import CookieJar, Cookie
 else:
-    from urllib import addinfourl, quote, unquote_plus, quote_plus
+    from urllib import quote, unquote_plus, quote_plus
     from urlparse import urlparse, parse_qs, urljoin
-    from urllib2 import urlopen, Request, HTTPError, URLError, build_opener, \
-                        HTTPCookieProcessor, HTTPRedirectHandler
-    from cookielib import CookieJar, Cookie
