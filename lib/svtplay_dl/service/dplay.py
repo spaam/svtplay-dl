68f300ec05c92e44aca6e0c7dbbc39c8e9e6dea2
Johan Andersson <j@i19.se>
2015-10-29 19:32:38 +0100

    dplay: better names for output file

--- lib/svtplay_dl/service/dplay.py
+++ lib/svtplay_dl/service/dplay.py
@@ -5,13 +5,14 @@ import re
 import copy
 import json
 import time
+import os
 
 from svtplay_dl.service import Service
 from svtplay_dl.fetcher.hds import hdsparse
 from svtplay_dl.fetcher.hls import hlsparse
 from svtplay_dl.utils.urllib import quote
 from svtplay_dl.error import ServiceError
-
+from svtplay_dl.utils import filenamify
 
 class Dplay(Service):
     supported_domains = ['dplay.se']
@@ -27,23 +28,49 @@ class Dplay(Service):
         if not match:
             yield ServiceError("Can't find video id")
             return
+        vid = match.group(1)
+        data = self.http.request("get", "http://www.dplay.se/api/v2/ajax/videos?video_id=%s" % vid).text
+        dataj = json.loads(data)
 
+        if dataj["data"][0]["content_info"]["package_label"]["value"] == "Premium":
+            yield ServiceError("Premium content")
+            return
+
+        if options.output_auto:
+            directory = os.path.dirname(options.output)
+            options.service = "dplay"
+            name = self._autoname(dataj)
+            if name is None:
+                yield ServiceError("Cant find vid id for autonaming")
+                return
+            title = "%s-%s-%s" % (name, vid, options.service)
+            if len(directory):
+                options.output = os.path.join(directory, title)
+            else:
+                options.output = title
         data = self.http.request("get", "http://geo.dplay.se/geo.js").text
         dataj = json.loads(data)
         geo = dataj["countryCode"]
         timestamp = (int(time.time())+3600)*1000
         cookie = {"dsc-geo": quote('{"countryCode":"%s","expiry":%s}' % (geo, timestamp))}
-        data = self.http.request("get", "https://secure.dplay.se/secure/api/v2/user/authorization/stream/%s?stream_type=hds" % match.group(1), cookies=cookie)
+        data = self.http.request("get", "https://secure.dplay.se/secure/api/v2/user/authorization/stream/%s?stream_type=hds" % vid, cookies=cookie)
         dataj = json.loads(data.text)
         if "hds" in dataj:
             streams = hdsparse(copy.copy(options), self.http.request("get", dataj["hds"], params={"hdcore": "3.8.0"}), dataj["hds"])
             if streams:
                 for n in list(streams.keys()):
                     yield streams[n]
-        data = self.http.request("get", "https://secure.dplay.se/secure/api/v2/user/authorization/stream/%s?stream_type=hls" % match.group(1), cookies=cookie)
+        data = self.http.request("get", "https://secure.dplay.se/secure/api/v2/user/authorization/stream/%s?stream_type=hls" % vid, cookies=cookie)
         dataj = json.loads(data.text)
         if "hls" in dataj:
             streams = hlsparse(options, self.http.request("get", dataj["hls"]), dataj["hls"])
             if streams:
                 for n in list(streams.keys()):
                     yield streams[n]
+
+    def _autoname(self, jsondata):
+        show = jsondata["data"][0]["taxonomy_items"][0]["slug"]
+        season = jsondata["data"][0]["season"]
+        episode = jsondata["data"][0]["episode"]
+        title = jsondata["data"][0]["title"]
+        return filenamify("%s.s%se%s.%s" % (show, season, episode, title))
