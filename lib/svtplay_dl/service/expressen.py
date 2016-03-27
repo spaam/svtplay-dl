f92e94a8d2cc465add15e499d6ca660b5dfb9271
Johan Andersson <j@i19.se>
2016-03-27 12:49:27 +0200

    expressen: update way to download the videos

--- lib/svtplay_dl/service/expressen.py
+++ lib/svtplay_dl/service/expressen.py
@@ -2,16 +2,13 @@
 # -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
 from __future__ import absolute_import
 import re
-import copy
-import xml.etree.ElementTree as ET
+import json
 
 from svtplay_dl.service import Service
 from svtplay_dl.error import ServiceError
 from svtplay_dl.log import log
 from svtplay_dl.fetcher.hls import hlsparse
-from svtplay_dl.fetcher.rtmp import RTMP
-from svtplay_dl.utils import is_py2_old
-from svtplay_dl.utils.urllib import unquote_plus
+from svtplay_dl.utils import decode_html_entities
 
 
 class Expressen(Service):
@@ -24,38 +21,24 @@ class Expressen(Service):
             yield ServiceError("Excluding video")
             return
 
-        match = re.search("xmlUrl=([^ ]+)\" ", data)
-        if match:
-            xmlurl = unquote_plus(match.group(1))
-        else:
-            match = re.search(
-                r"moviesList: \[\{\"VideoId\":\"(\d+)\"",
-                self.get_urldata())
-            if not match:
-                log.error("Can't find video id")
-                return
-            vid = match.group(1)
-            xmlurl = "http://www.expressen.se/Handlers/WebTvHandler.ashx?id=%s" % vid
-        data = self.http.request("get", xmlurl).content
+        match = re.search('="(http://www.expressen.se/tvspelare[^"]+)"', data)
+        if not match:
+            log.error("Can't find video id")
+            return
+        url = decode_html_entities(match.group(1))
+        data = self.http.request("get", url)
 
-        xml = ET.XML(data)
-        live = xml.find("live").text
-        if live != "0":
-            self.options.live = True
-        ss = xml.find("vurls")
-        if is_py2_old:
-            sa = list(ss.getiterator("vurl"))
-        else:
-            sa = list(ss.iter("vurl"))
+        match = re.search("window.Player.settings = ({.*});", data.text)
+        if not match:
+            log.error("Can't find json info.")
 
-        for i in sa:
-            options2 = copy.copy(self.options)
-            match = re.search(r"rtmp://([-0-9a-z\.]+/[-a-z0-9]+/)(.*)", i.text)
-            filename = "rtmp://%s" % match.group(1)
-            options2.other = "-y %s" % match.group(2)
-            yield RTMP(options2, filename, int(i.attrib["bitrate"]))
-
-        ipadurl = xml.find("mobileurls").find("ipadurl").text
-        streams = hlsparse(self.options, self.http.request("get", ipadurl), ipadurl)
-        for n in list(streams.keys()):
-            yield streams[n]
+        dataj = json.loads(match.group(1))
+        if "streams" in dataj:
+            if "iPad" in dataj["streams"]:
+                streams = hlsparse(self.options, self.http.request("get", dataj["streams"]["iPad"]), dataj["streams"]["iPad"])
+                for n in list(streams.keys()):
+                    yield streams[n]
+            if "hashHls" in dataj["streams"]:
+                streams = hlsparse(self.options, self.http.request("get", dataj["streams"]["hashHls"]), dataj["streams"]["hashHls"])
+                for n in list(streams.keys()):
+                    yield streams[n]
