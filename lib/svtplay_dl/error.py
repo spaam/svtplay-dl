51c71aa1cbe84a28fd31a467ad8dfd2e26bb63a6
Olof Johansson <olof@ethup.se>
2016-03-29 19:42:46 +0200

    error: New exception, NoRequestedProtocols
    This excpetion is thrown when the stream can't be accessed by any accepted
    protocol (as decided by options.stream_prio).

--- lib/svtplay_dl/error.py
+++ lib/svtplay_dl/error.py
@@ -8,3 +8,31 @@ class UIException(Exception):
 
 class ServiceError(Exception):
     pass
+
+class NoRequestedProtocols(UIException):
+    """
+    This excpetion is thrown when the service provides streams,
+    but not using any accepted protocol (as decided by
+    options.stream_prio).
+    """
+
+    def __init__(self, requested, found):
+        """
+        The constructor takes two mandatory parameters, requested
+        and found. Both should be lists. requested is the protocols
+        we want and found is the protocols that can be used to
+        access the stream.
+        """
+        self.requested = requested
+        self.found = found
+
+        super(NoRequestedProtocols, self).__init__(
+            "None of the provided protocols (%s) are in "
+            "the current list of accepted protocols (%s)" % (
+                self.found, self.requested
+            )
+        )
+
+    def __repr__(self):
+        return "NoRequestedProtocols(requested=%s, found=%s)" % (
+            self.requested, self.found)
