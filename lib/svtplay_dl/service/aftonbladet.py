84d13a9f0c642cd1fc603cfe51f64f874c3e00b0
Johan Andersson <j@i19.se>
2016-10-06 22:46:40 +0200

    aftonbladet: download videos again

--- lib/svtplay_dl/service/aftonbladet.py
+++ lib/svtplay_dl/service/aftonbladet.py
@@ -20,51 +20,23 @@ class Aftonbladet(Service):
             yield ServiceError("Excluding video")
             return
 
-        match = re.search('data-aptomaId="([-0-9a-z]+)"', data)
+        apiurl = None
+        match = re.search('data-player-config="([^"]+)"', data)
         if not match:
-            match = re.search('data-player-config="([^"]+)"', data)
-            if not match:
-                yield ServiceError("Can't find video info")
-                return
-            janson = json.loads(decode_html_entities(match.group(1)))
-            videoId = janson["aptomaVideoId"]
-        else:
-            videoId = match.group(1)
-            match = re.search(r'data-isLive="(\w+)"', data)
-            if not match:
-                yield ServiceError("Can't find live info")
-                return
-            if match.group(1) == "true":
-                self.options.live = True
+            yield ServiceError("Can't find video info")
+            return
+        janson = json.loads(decode_html_entities(match.group(1)))
 
+        videoId = janson["playerOptions"]["id"]
+        apiurl = janson["playerOptions"]["api"]
+        vendor = janson["playerOptions"]["vendor"]
+        self.options.live = janson["live"]
         if not self.options.live:
-            dataurl = "http://aftonbladet-play-metadata.cdn.drvideo.aptoma.no/video/%s.json" % videoId
+            dataurl = "{}{}/assets/{}?appName=svp-player".format(apiurl, vendor, videoId)
             data = self.http.request("get", dataurl).text
             data = json.loads(data)
-            videoId = data["videoId"]
 
-        streamsurl = "http://aftonbladet-play-static-ext.cdn.drvideo.aptoma.no/actions/video/?id=%s&formats&callback=" % videoId
-        data = self.http.request("get", streamsurl).text
-        streams = json.loads(data)
-        hlsstreams = streams["formats"]["hls"]
-        if "level3" in hlsstreams.keys():
-            hls = hlsstreams["level3"]
-        else:
-            hls = hlsstreams["akamai"]
-        if "csmil" in hls.keys():
-            hls = hls["csmil"][0]
-        else:
-            hls = hls["m3u8"][0]
-        address = hls["address"]
-        path = hls["path"]
-
-        for i in hls["files"]:
-            if "filename" in i.keys():
-                plist = "http://%s/%s/%s/master.m3u8" % (address, path, i["filename"])
-            else:
-                plist = "http://%s/%s/%s" % (address, path, hls["filename"])
-
-            streams = hlsparse(self.options, self.http.request("get", plist), plist)
+            streams = hlsparse(self.options, self.http.request("get", data["streamUrls"]["hls"]), data["streamUrls"]["hls"])
             if streams:
                 for n in list(streams.keys()):
                     yield streams[n]
