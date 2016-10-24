0ccef878012e37c6dc9d524752d0c579df97ca01
Linus Karlsson <linus@zozs.se>
2016-10-24 21:24:36 +0200

    subtitle.tt: fix error when calculating end time.

--- lib/svtplay_dl/subtitle/__init__.py
+++ lib/svtplay_dl/subtitle/__init__.py
@@ -91,7 +91,7 @@ class subtitle(object):
                         sec = float(begin2[2]) + float(duration2[2])
                     except ValueError:
                         sec = 0.000
-                    end = "%02d:%02d:%06.3f" % (int(begin[0]), int(begin[1]), sec)
+                    end = "%02d:%02d:%06.3f" % (int(begin2[0]), int(begin2[1]), sec)
                 else:
                     end = node.attrib["end"]
                 data += '%s\n%s --> %s\n' % (i, begin.replace(".", ","), end.replace(".", ","))
