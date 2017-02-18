8b09165cd753871809eebdfcaf5289d2d209847d
Johan Andersson <j@i19.se>
2017-02-18 23:51:58 +0100

    nhl: add support for the older platform

--- lib/svtplay_dl/service/nhl.py
+++ lib/svtplay_dl/service/nhl.py
@@ -5,6 +5,7 @@ import json
 from svtplay_dl.service import Service, OpenGraphThumbMixin
 from svtplay_dl.fetcher.hls import hlsparse
 from svtplay_dl.error import ServiceError
+from svtplay_dl.utils.urllib import urljoin
 
 
 class NHL(Service, OpenGraphThumbMixin):
@@ -19,6 +20,10 @@ class NHL(Service, OpenGraphThumbMixin):
             yield ServiceError("Cant find any media on that page")
             return
         janson = json.loads(match.group(1))
+        vid = janson["content_id"]
+        if not janson["metaData"]:
+            yield ServiceError("Can't find video on that page")
+            return
         if "playbacks" in janson["metaData"]:
             for i in janson["metaData"]["playbacks"]:
                 if "CLOUD" in i["name"]:
@@ -27,5 +32,23 @@ class NHL(Service, OpenGraphThumbMixin):
                         for n in list(streams.keys()):
                             yield streams[n]
         else:
-            yield ServiceError("Can't find any video metadata")
-            return
+            match = re.search("var mediaConfig\s+= ({[^;]+);", self.get_urldata())
+            if not match:
+                yield ServiceError("Cant find any media on that page")
+                return
+            janson = json.loads(match.group(1))
+            try:
+                apiurl = janson["vpm"]["mediaFramework"]["mediaFrameworkDomain"]
+            except KeyError:
+                yield ServiceError("Can't find api url")
+                return
+            filename = "{0}?contentId={1}&playbackScenario=HTTP_CLOUD_WIRED_WEB&format=json&platform=WEB_MEDIAPLAYER&_=1487455224334".format(janson["vpm"]["mediaFramework"]["mediaFrameworkEndPoint"], vid)
+            url = urljoin(apiurl, filename)
+            res = self.http.get(url)
+            janson = res.json()
+            for i in janson["user_verified_event"][0]["user_verified_content"][0]["user_verified_media_item"]:
+                streams = hlsparse(self.options, self.http.request("get", i["url"]), i["url"])
+                if streams:
+                    for n in list(streams.keys()):
+                        yield streams[n]
+
