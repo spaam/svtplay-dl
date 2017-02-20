4cd49f2a16676519a545de487bd0b1bbe496634b
Johan Andersson <j@i19.se>
2017-02-21 00:14:54 +0100

    New version 1.9.3

--- lib/svtplay_dl/__init__.py
+++ lib/svtplay_dl/__init__.py
@@ -55,7 +55,7 @@ from svtplay_dl.service.viasatsport import Viasatsport
 from svtplay_dl.service.vimeo import Vimeo
 from svtplay_dl.service.youplay import Youplay
 
-__version__ = "1.9.2"
+__version__ = "1.9.3"
 
 sites = [
     Aftonbladet,
