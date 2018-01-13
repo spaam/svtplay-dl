2283d3e5939383e4dbbeccea77e4fb6852ba2834
Johan Andersson <j@i19.se>
2018-01-13 21:21:49 +0100

    postprocess: fix indentation

--- lib/svtplay_dl/postprocess/__init__.py
+++ lib/svtplay_dl/postprocess/__init__.py
@@ -124,7 +124,7 @@ class postprocess(object):
                 for stream_num, language in enumerate(langs):
                     arguments += ["-map", str(stream_num + 1), "-c:s:" + str(stream_num), "mov_text",
                                   "-metadata:s:s:" + str(stream_num), "language=" + language]
-                if len(self.subfixes) >= 2:
+                if self.subfixes and len(self.subfixes) >= 2:
                     for subfix in self.subfixes:
                         subfile = "{0}.srt".format(name + subfix)
                         cmd += ["-i", subfile]
