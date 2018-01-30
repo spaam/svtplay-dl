b82831df032aee03456c0aa1bc93bbd81b46b04f
Johan Andersson <j@i19.se>
2018-01-30 21:45:54 +0100

    Move services to its own file

--- lib/svtplay_dl/utils/getmedia.py
+++ lib/svtplay_dl/utils/getmedia.py
@@ -5,101 +5,13 @@ import copy
 
 from svtplay_dl.log import log
 from svtplay_dl.service import service_handler, Generic
+from svtplay_dl.service.services import sites, Raw
 from svtplay_dl.fetcher import VideoRetriever
 from svtplay_dl.subtitle import subtitle
 from svtplay_dl.output import filename
 from svtplay_dl.postprocess import postprocess
 from svtplay_dl.utils import select_quality, list_quality
 from svtplay_dl.error import UIException
-
-
-from svtplay_dl.service.aftonbladet import Aftonbladet, Aftonbladettv
-from svtplay_dl.service.bambuser import Bambuser
-from svtplay_dl.service.bigbrother import Bigbrother
-from svtplay_dl.service.cmore import Cmore
-from svtplay_dl.service.dbtv import Dbtv
-from svtplay_dl.service.disney import Disney
-from svtplay_dl.service.dplay import Dplay
-from svtplay_dl.service.dr import Dr
-from svtplay_dl.service.efn import Efn
-from svtplay_dl.service.expressen import Expressen
-from svtplay_dl.service.facebook import Facebook
-from svtplay_dl.service.filmarkivet import Filmarkivet
-from svtplay_dl.service.flowonline import Flowonline
-from svtplay_dl.service.hbo import Hbo
-from svtplay_dl.service.twitch import Twitch
-from svtplay_dl.service.lemonwhale import Lemonwhale
-from svtplay_dl.service.mtvnn import Mtvnn
-from svtplay_dl.service.mtvservices import Mtvservices
-from svtplay_dl.service.nhl import NHL
-from svtplay_dl.service.nrk import Nrk
-from svtplay_dl.service.oppetarkiv import OppetArkiv
-from svtplay_dl.service.picsearch import Picsearch
-from svtplay_dl.service.pokemon import Pokemon
-from svtplay_dl.service.qbrick import Qbrick
-from svtplay_dl.service.radioplay import Radioplay
-from svtplay_dl.service.riksdagen import Riksdagen
-from svtplay_dl.service.ruv import Ruv
-from svtplay_dl.service.raw import Raw
-from svtplay_dl.service.solidtango import Solidtango
-from svtplay_dl.service.sportlib import Sportlib
-from svtplay_dl.service.sr import Sr
-from svtplay_dl.service.svt import Svt
-from svtplay_dl.service.barnkanalen import Barnkanalen
-from svtplay_dl.service.svtplay import Svtplay
-from svtplay_dl.service.tv4play import Tv4play
-from svtplay_dl.service.urplay import Urplay
-from svtplay_dl.service.vg import Vg
-from svtplay_dl.service.viaplay import Viaplay
-from svtplay_dl.service.viasatsport import Viasatsport
-from svtplay_dl.service.vidme import Vidme
-from svtplay_dl.service.vimeo import Vimeo
-from svtplay_dl.service.youplay import Youplay
-
-sites = [
-    Aftonbladet,
-    Aftonbladettv,
-    Bambuser,
-    Barnkanalen,
-    Bigbrother,
-    Cmore,
-    Dbtv,
-    Disney,
-    Dplay,
-    Dr,
-    Efn,
-    Expressen,
-    Facebook,
-    Filmarkivet,
-    Flowonline,
-    Hbo,
-    Twitch,
-    Lemonwhale,
-    Mtvservices,
-    Mtvnn,
-    NHL,
-    Nrk,
-    Qbrick,
-    Picsearch,
-    Pokemon,
-    Ruv,
-    Radioplay,
-    Solidtango,
-    Sportlib,
-    Sr,
-    Svt,
-    Svtplay,
-    OppetArkiv,
-    Tv4play,
-    Urplay,
-    Viaplay,
-    Viasatsport,
-    Vidme,
-    Vimeo,
-    Vg,
-    Youplay,
-    Riksdagen,
-    Raw]
 
 
 def get_multiple_media(urls, options):
