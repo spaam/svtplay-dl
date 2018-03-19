5db5371171cc19e2398a2d1d9180fe5bdb9365de
dalgr <dalgr.hean@gmail.com>
2018-03-19 20:51:38 +0100

    Add support for hls fetching from vimeo

--- lib/svtplay_dl/service/vimeo.py
+++ lib/svtplay_dl/service/vimeo.py
@@ -9,6 +9,8 @@ from svtplay_dl.service import Service, OpenGraphThumbMixin
 from svtplay_dl.fetcher.http import HTTP
 from svtplay_dl.error import ServiceError
 
+from svtplay_dl.fetcher.hls import hlsparse
+
 
 class Vimeo(Service, OpenGraphThumbMixin):
     supported_domains = ['vimeo.com']
@@ -37,10 +39,21 @@ class Vimeo(Service, OpenGraphThumbMixin):
         player_data = self.http.request("get", player_url).text
 
         if player_data:
+
             jsondata = json.loads(player_data)
+
+            if ("hls" in jsondata["request"]["files"]) and ("fastly_skyfire" in jsondata["request"]["files"]["hls"]["cdns"]):
+                hls_elem = jsondata["request"]["files"]["hls"]["cdns"]["fastly_skyfire"]
+                stream = hlsparse(self.options, self.http.request("get", hls_elem["url"]), hls_elem["url"])
+
+                if stream:
+                    for n in list(stream.keys()):
+                        yield stream[n]
+
             avail_quality = jsondata["request"]["files"]["progressive"]
             for i in avail_quality:
                 yield HTTP(copy.copy(self.options), i["url"], i["height"])
+
         else:
             yield ServiceError("Can't find any streams.")
             return
