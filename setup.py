38e29e164f00a37a08fa536e1b04dce68720aa46
Johan Andersson <j@i19.se>
2018-05-21 00:00:23 +0200

    setup: remove 2.7 from classifiers

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
                  "Environment :: Console",
                  "Operating System :: POSIX",
                  "Operating System :: Microsoft :: Windows",
-                 "Programming Language :: Python :: 2.7",
                  "Programming Language :: Python :: 3.4",
                  "Programming Language :: Python :: 3.5",
                  "Programming Language :: Python :: 3.6",
