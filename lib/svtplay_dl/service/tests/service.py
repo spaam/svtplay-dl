f183c3014e0bf4eee211d09877306a1f4b4a4007
Johan Andersson <j@i19.se>
2018-10-05 23:46:52 +0200

    service.test: add some opengraph_get test cases

--- lib/svtplay_dl/service/tests/service.py
+++ lib/svtplay_dl/service/tests/service.py
@@ -7,7 +7,7 @@
 
 from __future__ import absolute_import
 import unittest
-from svtplay_dl.service import Service, service_handler
+from svtplay_dl.service import Service, service_handler, opengraph_get
 from svtplay_dl.service.services import sites
 from svtplay_dl.utils.parser import setup_defaults
 
@@ -29,7 +29,29 @@ class service_handlerTest(unittest.TestCase):
         config = setup_defaults()
         self.assertIsNone(service_handler(sites, config, "localhost"))
 
+
 class service_handlerTest2(unittest.TestCase):
     def test_service_handler(self):
         config = setup_defaults()
         self.assertIsInstance(service_handler(sites, config, "https://www.svtplay.se"), Service)
+
+
+class service_opengraphGet(unittest.TestCase):
+    text = '<html><head><meta name="og:image" property="og:image" content="http://example.com/img3.jpg"><meta'
+
+    def test_og_get(self):
+        self.assertEqual(opengraph_get(self.text, "image"), "http://example.com/img3.jpg")
+
+
+class service_opengraphGet_none(unittest.TestCase):
+    text = '<html><head><meta name="og:image" property="og:image" content="http://example.com/img3.jpg"><meta'
+
+    def test_og_get(self):
+        self.assertIsNone(opengraph_get(self.text, "kalle"))
+
+
+class service_opengraphGet2(unittest.TestCase):
+    text = '<html><head><meta name="og:image" property="og:image" content="http://example.com/img3.jpg">'
+
+    def test_og_get(self):
+        self.assertEqual(opengraph_get(self.text, "image"), "http://example.com/img3.jpg")
