9678370dcef6e77d0e4f9ebed40c2cab0bd33054
Johan Andersson <j@i19.se>
2019-08-31 14:59:15 +0200

    Add a bunch of subtitle function unittests

--- lib/svtplay_dl/tests/test_subtitle.py
+++ lib/svtplay_dl/tests/test_subtitle.py
@@ -6,6 +6,7 @@
 from __future__ import absolute_import
 
 import unittest
+import xml.etree.ElementTree as ET
 
 import svtplay_dl.subtitle
 
@@ -28,3 +29,63 @@ class timestrTest(unittest.TestCase):
 
     def test_3600000(self):
         assert svtplay_dl.subtitle.timestr(3600000) == "01:00:00,000"
+
+
+class strdateTest(unittest.TestCase):
+    def test_match(self):
+        assert svtplay_dl.subtitle.strdate("21:33:30.33 --> 21:33:33.50")
+
+    def test_notmatch(self):
+        assert not svtplay_dl.subtitle.strdate("21:33:30.33 --> 21:33.33.50")
+
+
+class sec2str(unittest.TestCase):
+    def test_sec3600(self):
+        assert svtplay_dl.subtitle.sec2str(3600) == "01:00:00.000"
+
+    def test_sec3650(self):
+        assert svtplay_dl.subtitle.sec2str(3650) == "01:00:50.000"
+
+    def test_sec3002(self):
+        assert svtplay_dl.subtitle.sec2str(3002.23) == "00:50:02.230"
+
+
+class str2sec(unittest.TestCase):
+    def test_str3600(self):
+        assert svtplay_dl.subtitle.str2sec("01:01:01.000") == 3661.0
+
+    def test_str3650(self):
+        assert svtplay_dl.subtitle.str2sec("01:00:50.000") == 3650.0
+
+    def test_str3002(self):
+        assert svtplay_dl.subtitle.str2sec("00:50:02.230") == 3002.23
+
+
+class timecolon(unittest.TestCase):
+    def test_timecolon(self):
+        assert svtplay_dl.subtitle.timecolon("00:50:02:230") == "00:50:02,230"
+
+
+class normr(unittest.TestCase):
+    def test_norm1(self):
+        assert svtplay_dl.subtitle.norm("kalle") == "kalle"
+
+    def test_norm2(self):
+        assert svtplay_dl.subtitle.norm("{kalle}anka") == "anka"
+
+
+class tt_text(unittest.TestCase):
+    def test_tt_text1(self):
+        assert svtplay_dl.subtitle.tt_text(ET.fromstring("<tests>kalle</tests>"), "") == "kalle\n"
+
+    def test_tt_text2(self):
+        assert svtplay_dl.subtitle.tt_text(ET.fromstring("<tests><test1>kalle</test1><test2>anka</test2></tests>"), "") == "kalle\nanka\n"
+
+    def test_tt_text3(self):
+        assert svtplay_dl.subtitle.tt_text(ET.fromstring("<tests><test1>hej</test1>kalle</tests>"), "") == "hej\nkalle\n"
+
+    def test_tt_text4(self):
+        assert svtplay_dl.subtitle.tt_text(ET.fromstring("<tests>kalle<test2>hej</test2></tests>"), "") == "kalle\nhej\n"
+
+    def test_tt_text5(self):
+        assert svtplay_dl.subtitle.tt_text(ET.fromstring("<tests><test>kalle</test>hej</tests>"), "") == "kalle\nhej\n"
