8a2bd08b25a1b0ca9a14de08092d6b3ce8952a75
Johan Andersson <j@i19.se>
2021-06-06 23:13:28 +0200

    dr: update so it works with the website
    fixes: 1394

--- lib/svtplay_dl/service/dr.py
+++ lib/svtplay_dl/service/dr.py
@@ -23,13 +23,17 @@ class Dr(Service, OpenGraphThumbMixin):
             return
         janson = json.loads(match.group(1))
         page = janson["cache"]["page"][list(janson["cache"]["page"].keys())[0]]
-        offers = page["entries"][0]["item"]["offers"]
         resolution = None
         vid = None
+        if "item" in page["entries"][0]:
+            offers = page["entries"][0]["item"]["offers"]
+        elif "item" in page:
+            offers = page["item"]["offers"]
+
+        offerlist = []
         for i in offers:
             if i["deliveryType"] == "Stream":
-                vid = i["scopes"][0]
-                resolution = i["resolution"]
+                offerlist.append([i["scopes"][0], i["resolution"]])
 
         deviceid = uuid.uuid1()
         res = self.http.request(
@@ -39,23 +43,27 @@ class Dr(Service, OpenGraphThumbMixin):
         )
         token = res.json()[0]["value"]
 
-        url = "https://isl.dr-massive.com/api/account/items/{}/videos?delivery=stream&device=web_browser&ff=idp%2Cldp&lang=da&resolution={}&sub=Anonymous".format(
-            vid,
-            resolution,
-        )
-        res = self.http.request("get", url, headers={"authorization": f"Bearer {token}"})
-        for video in res.json():
-            if video["accessService"] == "StandardVideo":
-                if video["format"] == "video/hls":
-                    res = self.http.request("get", video["url"])
-                    if res.status_code > 400:
-                        yield ServiceError("Can't play this because the video is geoblocked or not available.")
-                    else:
-                        yield from hlsparse(self.config, res, video["url"], output=self.output)
-                        yield subtitle(copy.copy(self.config), "wrst", video["subtitles"][0]["link"], output=self.output)
+        if len(offerlist) == 0:
+            yield ServiceError("Can't find any videos")
+            return
+
+        for i in offerlist:
+            vid, resolution = i
+            url = f"https://isl.dr-massive.com/api/account/items/{vid}/videos?delivery=stream&device=web_browser&ff=idp%2Cldp&lang=da&resolution={resolution}&sub=Anonymous"
+            res = self.http.request("get", url, headers={"authorization": f"Bearer {token}"})
+            for video in res.json():
+                if video["accessService"] == "StandardVideo":
+                    if video["format"] == "video/hls":
+                        res = self.http.request("get", video["url"])
+                        if res.status_code > 400:
+                            yield ServiceError("Can't play this because the video is geoblocked or not available.")
+                        else:
+                            yield from hlsparse(self.config, res, video["url"], output=self.output)
+                            yield subtitle(copy.copy(self.config), "wrst", video["subtitles"][0]["link"], output=self.output)
 
     def find_all_episodes(self, config):
         episodes = []
+        seasons = []
         data = self.get_urldata()
         match = re.search("__data = ([^<]+)</script>", data)
         if not match:
@@ -63,14 +71,33 @@ class Dr(Service, OpenGraphThumbMixin):
             return episodes
         janson = json.loads(match.group(1))
         page = janson["cache"]["page"][list(janson["cache"]["page"].keys())[0]]
-        item = page["entries"][0]["item"]
-        if "season" in item:
-            entries = item["season"]["episodes"]["items"]
-            for i in entries:
-                episodes.append("https://www.dr.dk/drtv{}".format(i["watchPath"]))
 
-            if config.get("all_last") != -1:
-                episodes = episodes[: config.get("all_last")]
-            else:
-                episodes.reverse()
+        if "show" in page["item"] and "seasons" in page["item"]["show"]:
+            for i in page["item"]["show"]["seasons"]["items"]:
+                seasons.append(f'https://www.dr.dk/drtv{i["path"]}')
+
+        if seasons:
+            for season in seasons:
+                data = self.http.get(season).text
+                match = re.search("__data = ([^<]+)</script>", data)
+                janson = json.loads(match.group(1))
+                page = janson["cache"]["page"][list(janson["cache"]["page"].keys())[0]]
+                episodes.extend(self._get_episodes(page))
+        else:
+            episodes.extend(self._get_episodes(page))
+
+        if config.get("all_last") != -1:
+            episodes = episodes[: config.get("all_last")]
+        else:
+            episodes.reverse()
+
+        return episodes
+
+    def _get_episodes(self, page):
+        episodes = []
+        if "episodes" in page["item"]:
+            entries = page["item"]["episodes"]["items"]
+            for i in entries:
+                episodes.append(f'https://www.dr.dk/drtv{i["watchPath"]}')
+
         return episodes
