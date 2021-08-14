6a13d157150d402be799a451e706b510d52cb03d
Johan Andersson <j@i19.se>
2021-08-14 15:16:21 +0200

    test: add some m3u8 test files

--- lib/svtplay_dl/tests/test_hls.py
+++ lib/svtplay_dl/tests/test_hls.py
@@ -5,9 +5,25 @@
 #   pylint: disable-msg=C0103
 # We're a test, we go where ever we want (within reason, of course):
 #   pylint: disable-msg=protected-access
+import os
 import unittest
 
+import requests_mock
+from svtplay_dl import fetcher
+from svtplay_dl.fetcher.hls import _hlsparse
 from svtplay_dl.fetcher.hls import M3U8
+from svtplay_dl.utils.parser import setup_defaults
+
+
+def parse(playlist):
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "m3u8-playlists", playlist)) as fd:
+        manifest = fd.read()
+    streams = {}
+    for i in list(_hlsparse(setup_defaults(), manifest, "http://localhost.com/", {})):
+        if isinstance(i, fetcher.VideoRetriever):
+            streams[i.bitrate] = i
+    return streams
+
 
 # Example HLS playlist, source:
 # loosly inspired by
@@ -29,6 +45,24 @@ something4/else.m3u8
 
 #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=41457,CODECS="mp4a.40.2"
 something0/else.m3u8
+"""
+
+M3U_EXAMPLE2 = """#EXTM3U
+#EXT-X-VERSION:4
+## Created with Unified Streaming Platform(version=1.9.5)
+#EXT-X-PLAYLIST-TYPE:VOD
+#EXT-X-MEDIA-SEQUENCE:1
+#EXT-X-INDEPENDENT-SEGMENTS
+#EXT-X-TARGETDURATION:60
+#USP-X-TIMESTAMP-MAP:MPEGTS=900000,LOCAL=1970-01-01T00:00:00Z
+#EXTINF:60, no desc
+pid200028961_3910568(3910568_ISMUSP)-textstream_swe=1000-1.webvtt
+#EXTINF:60, no desc
+pid200028961_3910568(3910568_ISMUSP)-textstream_swe=1000-2.webvtt
+#EXTINF:60, no desc
+pid200028961_3910568(3910568_ISMUSP)-textstream_swe=1000-3.webvtt
+#EXTINF:60, no desc
+pid200028961_3910568(3910568_ISMUSP)-textstream_swe=1000-4.webvtt
 """
 
 
@@ -74,3 +108,25 @@ class HlsTest(unittest.TestCase):
             # More examples can be found on "https://developer.apple.com/streaming/examples/"
         ]:
             assert M3U8(test["playlist"]).master_playlist == test["expected"]
+
+
+def test_noaudio_segment():
+    with requests_mock.Mocker() as m:
+        m.get("http://localhost.com/pid200028961_3910568(3910568_ISMUSP)-textstream_swe=1000.m3u8", text=M3U_EXAMPLE2)
+        data = parse("no-audio-uri.m3u8")
+        assert data[3642].segments is False
+        assert data[3642].audio is None
+
+
+def test_audio_top():
+    with requests_mock.Mocker() as m:
+        m.get("http://localhost.com/text/text-0.m3u8", text=M3U_EXAMPLE2)
+        data = parse("audio-uri-top.m3u8")
+        assert data[3295].segments
+        assert data[3295].audio
+
+
+def test_audio_bottom():
+    data = parse("audio-uri-bottom.m3u8")
+    assert data[2639].segments
+    assert data[2639].audio
