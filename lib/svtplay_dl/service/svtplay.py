f97d838412d22104f1d6ba8ed2094f0b3d884c88
Johan Andersson <j@i19.se>
2021-10-30 12:37:52 +0200

    svtplay: revert change to only use cmaf files

--- lib/svtplay_dl/service/svtplay.py
+++ lib/svtplay_dl/service/svtplay.py
@@ -109,7 +109,7 @@ class Svtplay(Service, MetadataThumbMixin):
                 return
 
             for i in janson["videoReferences"]:
-                if i["url"].find(".m3u8") > 0 and "hls-cmaf" in i["format"]:
+                if i["url"].find(".m3u8") > 0:
                     yield from hlsparse(self.config, self.http.request("get", i["url"]), i["url"], self.output)
                 elif i["url"].find(".mpd") > 0:
                     yield from dashparse(self.config, self.http.request("get", i["url"]), i["url"], output=self.output)
