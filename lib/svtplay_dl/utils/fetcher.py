c54712592124e63b05fb1f152bb058fb2ad110d8
Johan Andersson <j@i19.se>
2022-06-18 21:23:37 +0200

    filter_files: increase the ratio to 0.7
    0.6 was to low because it was good enough for the first
    entry and the ads

--- lib/svtplay_dl/utils/fetcher.py
+++ lib/svtplay_dl/utils/fetcher.py
@@ -5,7 +5,7 @@ def filter_files(m3u8):
     files = []
     good = m3u8.media_segment[1]["URI"]
     for segment in m3u8.media_segment:
-        if SequenceMatcher(None, good, segment["URI"]).ratio() > 0.6:
+        if SequenceMatcher(None, good, segment["URI"]).ratio() > 0.7:
             files.append(segment)
     m3u8.media_segment = files
     return m3u8
