9d27fe86d162c92ca55cceafbe0678ee1f1f638c
Johan Andersson <j@i19.se>
2023-07-19 00:28:53 +0200

    tv4play: update again
    skipping clips. way to much effort to fix it

--- lib/svtplay_dl/service/tv4play.py
+++ lib/svtplay_dl/service/tv4play.py
@@ -3,10 +3,6 @@
 import json
 import logging
 import re
-from datetime import datetime
-from datetime import timedelta
-from urllib.parse import quote
-from urllib.parse import urlparse
 
 from svtplay_dl.error import ServiceError
 from svtplay_dl.fetcher.dash import dashparse
@@ -20,21 +16,6 @@ class Tv4play(Service, OpenGraphThumbMixin):
     supported_domains = ["tv4play.se"]
 
     def get(self):
-        parse = urlparse(self.url)
-        if parse.path[:8] == "/kanaler":
-            end_time_stamp = (datetime.utcnow() - timedelta(minutes=1, seconds=20)).replace(microsecond=0)
-            start_time_stamp = end_time_stamp - timedelta(minutes=1)
-
-            url = (
-                f"https://bbr-l2v.akamaized.net/live/{parse.path[9:]}/master.m3u8?in={start_time_stamp.isoformat()}&out={end_time_stamp.isoformat()}?"
-            )
-
-            self.config.set("live", True)
-            streams = hlsparse(self.config, self.http.request("get", url), url, output=self.output, hls_time_stamp=True)
-            for n in list(streams.keys()):
-                yield streams[n]
-            return
-
         token = self._login()
         if token is None:
             yield ServiceError("You need username / password.")
@@ -57,15 +38,16 @@ class Tv4play(Service, OpenGraphThumbMixin):
         what = jansson["props"]["apolloStateFromServer"]["ROOT_QUERY"][key_check]["__ref"]
 
         if what.startswith("Series:"):
-            seriesid = jansson["props"]["apolloStateFromServer"][what]["id"]
-            url = f"https://client-gateway.tv4.a2d.tv/graphql?operationName=suggestedEpisode&variables=%7B%22id%22%3A%22{seriesid}%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%2232df600a3e3efb1362bae9ff73a5e7f929e75c154effb38d7b3516c3985e38e0%22%7D%7D"
-            res = self.http.request("get", url, headers={"Client-Name": "tv4-web", "Client-Version": "4.0.0", "Content-Type": "application/json"})
-            vid = res.json()["data"]["series"]["suggestedEpisode"]["episode"]["id"]
+            yield ServiceError("Use the video page not the series page")
+            return
         else:
             vid = jansson["props"]["apolloStateFromServer"][what]["id"]
 
         url = f"https://playback2.a2d.tv/play/{vid}?service=tv4play&device=browser&protocol=hls%2Cdash&drm=widevine&browser=GoogleChrome&capabilities=live-drm-adstitch-2%2Cyospace3"
         res = self.http.request("get", url, headers={"Authorization": f"Bearer {token}"})
+        if res.status_code > 400:
+            yield ServiceError("Can't play this because the video is geoblocked.")
+            return
         jansson = res.json()
 
         item = jansson["metadata"]
@@ -128,30 +110,26 @@ class Tv4play(Service, OpenGraphThumbMixin):
         episodes = []
         items = []
 
-        showid, jansson = self._get_seriesid(self.get_urldata(), dict())
+        token = self._login()
+        if token is None:
+            logging.error("You need username / password.")
+            return episodes
+
+        showid, jansson, kind = self._get_seriesid(self.get_urldata(), dict())
         if showid is None:
             logging.error("Cant find any videos")
-            return
-
-        for season in jansson["props"]["apolloStateFromServer"][f"Series:{showid}"]["allSeasonLinks"]:
+            return episodes
+        if showid is False:
+            logging.error("Can't play this because the video is geoblocked.")
+            return episodes
+        if kind == "Movie":
+            return [f"https://www.tv4play.se/video/{showid}"]
+        jansson = self._graphdetails(showid)
+        for season in jansson["data"]["media"]["allSeasonLinks"]:
             graph_list = self._graphql(season["seasonId"])
             for i in graph_list:
                 if i not in items:
                     items.append(i)
-
-        if config.get("include_clips"):
-            if jansson["props"]["apolloStateFromServer"][f"Series:{showid}"]["hasPanels"]:
-                key_check = None
-                for key in jansson["props"]["apolloStateFromServer"][f"Series:{showid}"].keys():
-                    if key.startswith("panels("):
-                        key_check = key
-
-                if key_check:
-                    for item in jansson["props"]["apolloStateFromServer"][f"Series:{showid}"][key_check]["items"]:
-                        if item["__ref"].startswith("Clips"):
-                            graph_list = self._graphclips(jansson["props"]["apolloStateFromServer"][item["__ref"]]["id"])
-                            for clip in graph_list:
-                                episodes.append(f"https://www.tv4play.se/klipp/{clip}")
 
         items = sorted(items)
         for item in items:
@@ -164,60 +142,65 @@ class Tv4play(Service, OpenGraphThumbMixin):
     def _get_seriesid(self, data, jansson):
         match = self._getjson(data)
         if not match:
-            return None, jansson
+            return None, jansson, None
         jansson = json.loads(match.group(1))
         if "params" not in jansson["query"]:
-            return None, jansson
+            return None, jansson, None
         showid = jansson["query"]["params"][0]
         key_check = None
         for key in jansson["props"]["apolloStateFromServer"]["ROOT_QUERY"].keys():
             if key.startswith("media"):
                 key_check = key
         what = jansson["props"]["apolloStateFromServer"]["ROOT_QUERY"][key_check]["__ref"]
+
         if what.startswith("Episode"):
+            if "series" not in jansson["props"]["apolloStateFromServer"][what]:
+                return False, jansson, what[: what.index(":")]
             series = jansson["props"]["apolloStateFromServer"][what]["series"]["__ref"].replace("Series:", "")
             res = self.http.request("get", f"https://www.tv4play.se/program/{series}/")
             showid, jansson = self._get_seriesid(res.text, jansson)
-        return showid, jansson
+        return showid, jansson, what[: what.index(":")]
+
+    def _graphdetails(self, show):
+        data = {
+            "operationName": "ContentDetailsPage",
+            "query": "query ContentDetailsPage($programId: ID!, $recommendationsInput: MediaRecommendationsInput!, $seriesSeasonInput: SeriesSeasonInput!) {\n  media(id: $programId) {\n    __typename\n    ... on Movie {\n      __typename\n      id\n      title\n      genres\n      slug\n      productionYear\n      progress {\n        __typename\n        percent\n        position\n      }\n      productionCountries {\n        __typename\n        countryCode\n        name\n      }\n      playableFrom {\n        __typename\n        isoString\n        humanDateTime\n      }\n      playableUntil {\n        __typename\n        isoString\n        humanDateTime\n        readableDistance(type: DAYS_LEFT)\n      }\n      video {\n        __typename\n        ...VideoFields\n      }\n      parentalRating {\n        __typename\n        ...ParentalRatingFields\n      }\n      credits {\n        __typename\n        ...MovieCreditsFields\n      }\n      label {\n        __typename\n        ...LabelFields\n      }\n      images {\n        __typename\n        main16x7 {\n          __typename\n          ...ImageFieldsLight\n        }\n        main16x9 {\n          __typename\n          ...ImageFieldsFull\n        }\n        poster2x3 {\n          __typename\n          ...ImageFieldsLight\n        }\n        logo {\n          __typename\n          ...ImageFieldsLight\n        }\n      }\n      synopsis {\n        __typename\n        brief\n        long\n        medium\n        short\n      }\n      trailers {\n        __typename\n        mp4\n        webm\n      }\n      recommendations(input: $recommendationsInput) {\n        __typename\n        pageInfo {\n          __typename\n          ...PageInfoFields\n        }\n        items {\n          __typename\n          ...RecommendedSeriesMediaItem\n          ...RecommendedMovieMediaItem\n        }\n      }\n      hasPanels\n      isPollFeatureEnabled\n      humanCallToAction\n      upsell {\n        __typename\n        tierId\n      }\n    }\n    ... on Series {\n      __typename\n      id\n      title\n      numberOfAvailableSeasons\n      genres\n      category\n      slug\n      hasPanels\n      isPollFeatureEnabled\n      upsell {\n        __typename\n        tierId\n      }\n      cdpPageOverride {\n        __typename\n        id\n      }\n      upcomingEpisode {\n        __typename\n        ...UpcomingEpisodeFields\n      }\n      trailers {\n        __typename\n        mp4\n        webm\n      }\n      parentalRating {\n        __typename\n        ...ParentalRatingFields\n      }\n      credits {\n        __typename\n        ...SeriesCreditsFields\n      }\n      label {\n        __typename\n        ...LabelFields\n      }\n      images {\n        __typename\n        main16x7 {\n          __typename\n          ...ImageFieldsLight\n        }\n        main16x9 {\n          __typename\n          ...ImageFieldsFull\n        }\n        poster2x3 {\n          __typename\n          ...ImageFieldsLight\n        }\n        logo {\n          __typename\n          ...ImageFieldsLight\n        }\n      }\n      synopsis {\n        __typename\n        brief\n        long\n      }\n      allSeasonLinks {\n        __typename\n        seasonId\n        title\n        numberOfEpisodes\n      }\n      seasonLinks(seriesSeasonInput: $seriesSeasonInput) {\n        __typename\n        items {\n          __typename\n          seasonId\n          numberOfEpisodes\n        }\n      }\n      suggestedEpisode {\n        __typename\n        humanCallToAction\n        episode {\n          __typename\n          id\n          playableFrom {\n            __typename\n            isoString\n          }\n          playableUntil {\n            __typename\n            isoString\n          }\n          progress {\n            __typename\n            percent\n            position\n          }\n          video {\n            __typename\n            ...VideoFields\n          }\n        }\n      }\n      recommendations(input: $recommendationsInput) {\n        __typename\n        pageInfo {\n          __typename\n          ...PageInfoFields\n        }\n        items {\n          __typename\n          ...RecommendedSeriesMediaItem\n          ...RecommendedMovieMediaItem\n        }\n      }\n    }\n    ... on SportEvent {\n      __typename\n      id\n      league\n      arena\n      country\n      round\n      inStudio\n      commentators\n      access {\n        __typename\n        hasAccess\n      }\n      title\n      productionYear\n      images {\n        __typename\n        main16x7 {\n          __typename\n          ...ImageFieldsFull\n        }\n        main16x9 {\n          __typename\n          ...ImageFieldsFull\n        }\n        poster2x3 {\n          __typename\n          ...ImageFieldsLight\n        }\n      }\n      trailers {\n        __typename\n        mp4\n      }\n      synopsis {\n        __typename\n        brief\n        short\n        long\n        medium\n      }\n      playableFrom {\n        __typename\n        isoString\n        humanDateTime\n      }\n      playableUntil {\n        __typename\n        isoString\n        humanDateTime\n        readableDistance(type: DAYS_LEFT)\n      }\n      liveEventEnd {\n        __typename\n        isoString\n      }\n      isLiveContent\n    }\n  }\n}\nfragment VideoFields on Video {\n  __typename\n  duration {\n    __typename\n    readableShort\n    seconds\n  }\n  id\n  isDrmProtected\n  isLiveContent\n  vimondId\n  access {\n    __typename\n    hasAccess\n  }\n}\nfragment ParentalRatingFields on ParentalRating {\n  __typename\n  finland {\n    __typename\n    ageRestriction\n    reason\n  }\n  sweden {\n    __typename\n    ageRecommendation\n    suitableForChildren\n  }\n}\nfragment MovieCreditsFields on MovieCredits {\n  __typename\n  actors {\n    __typename\n    characterName\n    name\n    type\n  }\n  directors {\n    __typename\n    name\n    type\n  }\n}\nfragment LabelFields on Label {\n  __typename\n  airtime\n  announcement\n  contentDetailsPage\n  recurringBroadcast\n}\nfragment ImageFieldsLight on Image {\n  __typename\n  source\n}\nfragment ImageFieldsFull on Image {\n  __typename\n  source\n  meta {\n    __typename\n    muteBgColor {\n      __typename\n      hex\n    }\n  }\n}\nfragment PageInfoFields on PageInfo {\n  __typename\n  hasNextPage\n  nextPageOffset\n  totalCount\n}\nfragment RecommendedSeriesMediaItem on RecommendedSeries {\n  __typename\n  series {\n    __typename\n    id\n    title\n    images {\n      __typename\n      cover2x3 {\n        __typename\n        source\n      }\n      main16x9 {\n        __typename\n        source\n        meta {\n          __typename\n          muteBgColor {\n            __typename\n            hex\n          }\n        }\n      }\n    }\n    label {\n      __typename\n      ...LabelFields\n    }\n    isPollFeatureEnabled\n  }\n}\nfragment RecommendedMovieMediaItem on RecommendedMovie {\n  __typename\n  movie {\n    __typename\n    id\n    title\n    images {\n      __typename\n      cover2x3 {\n        __typename\n        source\n      }\n      main16x9 {\n        __typename\n        source\n        meta {\n          __typename\n          muteBgColor {\n            __typename\n            hex\n          }\n        }\n      }\n    }\n    label {\n      __typename\n      ...LabelFields\n    }\n    isPollFeatureEnabled\n  }\n}\nfragment UpcomingEpisodeFields on UpcomingEpisode {\n  __typename\n  id\n  title\n  playableFrom {\n    __typename\n    humanDateTime\n    isoString\n  }\n  image {\n    __typename\n    main16x9 {\n      __typename\n      ...ImageFieldsLight\n    }\n  }\n}\nfragment SeriesCreditsFields on SeriesCredits {\n  __typename\n  directors {\n    __typename\n    name\n    type\n  }\n  hosts {\n    __typename\n    name\n    type\n  }\n  actors {\n    __typename\n    characterName\n    name\n    type\n  }\n}",
+            "variables": {
+                "programId": show,
+                "recommendationsInput": {"limit": 10, "offset": 0, "types": ["MOVIE", "SERIES"]},
+                "seriesSeasonInput": {"limit": 10, "offset": 0},
+            },
+        }
+        res = self.http.request(
+            "post",
+            "https://client-gateway.tv4.a2d.tv/graphql",
+            headers={"Client-Name": "tv4-web", "Client-Version": "4.0.0", "Content-Type": "application/json"},
+            json=data,
+        )
+        return res.json()
 
     def _graphql(self, show):
         items = []
         nr = 0
         total = 100
         while nr <= total:
-            variables = {"seasonId": show, "input": {"limit": 12, "offset": nr}}
-            querystring = f"operationName=SeasonEpisodes&variables={quote(json.dumps(variables, separators=(',', ':')))}&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%22ed1681cdf0f538949697babb57e34e399732046422df6ae60949c693362ca744%22%7D%7D"
+            data = {
+                "operationName": "SeasonEpisodes",
+                "query": "query SeasonEpisodes($seasonId: ID!, $input: SeasonEpisodesInput!) {\n  season(id: $seasonId) {\n    __typename\n    numberOfEpisodes\n    episodes(input: $input) {\n      __typename\n      initialSortOrder\n      pageInfo {\n        __typename\n        ...PageInfoFields\n      }\n      items {\n        __typename\n        ...EpisodeFields\n      }\n    }\n  }\n}\nfragment PageInfoFields on PageInfo {\n  __typename\n  hasNextPage\n  nextPageOffset\n  totalCount\n}\nfragment EpisodeFields on Episode {\n  __typename\n  id\n  title\n  playableFrom {\n    __typename\n    readableDistance\n    timestamp\n    isoString\n    humanDateTime\n  }\n  playableUntil {\n    __typename\n    readableDistance(type: DAYS_LEFT)\n    timestamp\n    isoString\n    humanDateTime\n  }\n  liveEventEnd {\n    __typename\n    isoString\n    humanDateTime\n    timestamp\n  }\n  progress {\n    __typename\n    percent\n    position\n  }\n  episodeNumber\n  synopsis {\n    __typename\n    short\n    brief\n    medium\n  }\n  seasonId\n  series {\n    __typename\n    id\n    title\n    images {\n      __typename\n      main16x9Annotated {\n        __typename\n        source\n      }\n    }\n  }\n  images {\n    __typename\n    main16x9 {\n      __typename\n      ...ImageFieldsFull\n    }\n  }\n  video {\n    __typename\n    ...VideoFields\n  }\n  isPollFeatureEnabled\n  parentalRating {\n    __typename\n    finland {\n      __typename\n      ageRestriction\n      reason\n      containsProductPlacement\n    }\n  }\n}\nfragment ImageFieldsFull on Image {\n  __typename\n  source\n  meta {\n    __typename\n    muteBgColor {\n      __typename\n      hex\n    }\n  }\n}\nfragment VideoFields on Video {\n  __typename\n  duration {\n    __typename\n    readableShort\n    seconds\n  }\n  id\n  isDrmProtected\n  isLiveContent\n  vimondId\n  access {\n    __typename\n    hasAccess\n  }\n}",
+                "variables": {"input": {"limit": 16, "offset": nr, "sortOrder": "ASC"}, "seasonId": show},
+            }
 
             res = self.http.request(
-                "get",
-                f"https://client-gateway.tv4.a2d.tv/graphql?{querystring}",
+                "post",
+                "https://client-gateway.tv4.a2d.tv/graphql",
                 headers={"Client-Name": "tv4-web", "Client-Version": "4.0.0", "Content-Type": "application/json"},
+                json=data,
             )
             janson = res.json()
-
+            # logging.info(json.dumps(janson, indent=2))
             total = janson["data"]["season"]["episodes"]["pageInfo"]["totalCount"]
             for mediatype in janson["data"]["season"]["episodes"]["items"]:
                 items.append(mediatype["id"])
-            nr += 12
-        return items
-
-    def _graphclips(self, show):
-        items = []
-        nr = 0
-        total = 100
-        while nr <= total:
-            variables = {"panelId": show, "offset": nr, "limit": 8}
-            querystring = f"operationName=Panel&variables={quote(json.dumps(variables, separators=(',', ':')))}&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%22843e9c11ac0512999fecd7646090d2e358c09ef30a4688d948d69dea17b82967%22%7D%7D"
-            res = self.http.request(
-                "get",
-                f"https://client-gateway.tv4.a2d.tv/graphql?{querystring}",
-                headers={"Client-Name": "tv4-web", "Client-Version": "4.0.0", "Content-Type": "application/json"},
-            )
-            janson = res.json()
-
-            total = janson["data"]["panel"]["content"]["pageInfo"]["totalCount"]
-            for mediatype in janson["data"]["panel"]["content"]["items"]:
-                items.append(mediatype["clip"]["id"])
             nr += 12
         return items
 
