5420cff4edd059843a12bd532d4262b3b1041690
Johan Andersson <j@i19.se>
2023-11-29 00:08:37 +0100

    pluto: update

--- lib/svtplay_dl/service/plutotv.py
+++ lib/svtplay_dl/service/plutotv.py
@@ -1,4 +1,5 @@
 import datetime
+import logging
 import re
 import uuid
 from urllib.parse import urlparse
@@ -12,6 +13,7 @@ from svtplay_dl.service import Service
 class Plutotv(Service, OpenGraphThumbMixin):
     supported_domains = ["pluto.tv"]
     urlreg = r"/on-demand/(movies|series)/([^/]+)(/season/\d+/episode/([^/]+))?"
+    urlreg2 = r"/on-demand/(movies|series)/([^/]+)(/episode/([^/]+))?"
 
     def get(self):
         self.data = self.get_urldata()
@@ -24,30 +26,30 @@ class Plutotv(Service, OpenGraphThumbMixin):
 
         self.slug = urlmatch.group(2)
         episodename = urlmatch.group(4)
+        if episodename is None:
+            urlmatch = re.search(self.urlreg2, parse.path)
+            if not urlmatch:
+                yield ServiceError("Can't find what video it is or live is not supported")
+                return
+            self.slug = urlmatch.group(2)
+            episodename = urlmatch.group(4)
         self._janson()
         HLSplaylist = None
-        found = False
 
-        servicevod = f"https://service-vod.clusters.pluto.tv/v4/vod/slugs/{self.slug}"
-        res = self.http.request("get", servicevod, params=self.query, headers={"Authorization": f"Bearer {self.sessionToken}"})
-        janson2 = res.json()
-        if janson2["type"] == "series":
-            self.output["title"] = janson2["name"]
-            for season in janson2["seasons"]:
-                for episode in season["episodes"]:
-                    if episode["slug"] == episodename and not found:
-                        self.output["season"] = episode["season"]
-                        self.output["episode"] = episode["number"]
-                        for stich in episode["stitched"]["paths"]:
-                            if stich["type"] == "hls":
-                                HLSplaylist = f"{self.mediaserver}{stich['path']}?{self.stitcherParams}"
-                                if self.http.request("get", HLSplaylist, headers={"Authorization": f"Bearer {self.sessionToken}"}).status_code < 400:
-                                    found = True
-        else:
-            self.output["title"] == janson2["name"]
-            for stich in janson2["stitched"]["paths"]:
-                if stich["type"] == "hls":
-                    HLSplaylist = f"{self.mediaserver}{stich['path']}?{self.stitcherParams}"
+        for vod in self.janson["VOD"]:
+            self.output["title"] = vod["name"]
+            if "seasons" in vod:
+                for season in vod["seasons"]:
+                    if "episodes" in season:
+                        for episode in season["episodes"]:
+                            if episode["_id"] == episodename:
+                                self.output["season"] = season["number"]
+                                self.output["episodename"] = episode["name"]
+                                for stich in episode["stitched"]["paths"]:
+                                    if stich["type"] == "hls":
+                                        HLSplaylist = f"{self.mediaserver}{stich['path']}?{self.stitcherParams}"
+                                        if self.http.request("get", HLSplaylist).status_code < 400:
+                                            break
 
         if not HLSplaylist:
             yield ServiceError("Can't find video info")
@@ -66,6 +68,9 @@ class Plutotv(Service, OpenGraphThumbMixin):
         self.data = self.get_urldata()
         parse = urlparse(self.url)
         urlmatch = re.search(self.urlreg, parse.path)
+        if urlmatch is None:
+            logging.error("Can't find what video it is or live is not supported")
+            return episodes
         if urlmatch.group(1) != "series":
             return episodes
         self.slug = urlmatch.group(2)
@@ -74,13 +79,13 @@ class Plutotv(Service, OpenGraphThumbMixin):
         match = re.search(r"^/([^\/]+)/", parse.path)
         language = match.group(1)
 
-        servicevod = f"https://service-vod.clusters.pluto.tv/v4/vod/slugs/{self.slug}"
-        res = self.http.request("get", servicevod, params=self.query, headers={"Authorization": f"Bearer {self.sessionToken}"})
-        janson2 = res.json()
-        for season in janson2["seasons"]:
-            seasonnr = season["number"]
-            for episode in season["episodes"]:
-                episodes.append(f"https://pluto.tv/{language}/on-demand/series/{self.slug}/season/{seasonnr}/episode/{episode['slug']}")
+        for vod in self.janson["VOD"]:
+            if "seasons" in vod:
+                for season in vod["seasons"]:
+                    seasonnr = season["number"]
+                    if "episodes" in season:
+                        for episode in season["episodes"]:
+                            episodes.append(f"https://pluto.tv/{language}/on-demand/series/{self.slug}/season/{seasonnr}/episode/{episode['_id']}")
         return episodes
 
     def _janson(self) -> None:
@@ -88,20 +93,20 @@ class Plutotv(Service, OpenGraphThumbMixin):
         self.query = {
             "appName": "web",
             "appVersion": self.appversion.group(1) if self.appversion else "na",
-            "deviceVersion": "100.0.0",
+            "deviceVersion": "119.0.0",
             "deviceModel": "web",
             "deviceMake": "firefox",
             "deviceType": "web",
             "clientID": uuid.uuid1(),
             "clientModelNumber": "1.0.0",
-            "episodeSlugs": self.slug,
+            "seriesIDs": self.slug,
             "serverSideAds": "false",
             "constraints": "",
             "drmCapabilities": "widevine%3AL3",
             "clientTime": datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
         }
         res = self.http.request("get", "https://boot.pluto.tv/v4/start", params=self.query)
-        janson = res.json()
-        self.mediaserver = janson["servers"]["stitcher"]
-        self.stitcherParams = janson["stitcherParams"]
-        self.sessionToken = janson["sessionToken"]
+        self.janson = res.json()
+        self.mediaserver = self.janson["servers"]["stitcher"]
+        self.stitcherParams = self.janson["stitcherParams"]
+        self.sessionToken = self.janson["sessionToken"]
