74b22d7ab04a2d9454a6b3d977a32a92e4b822db
filipelander <filipelander.elander@sponswatch.com>
2024-10-23 18:02:08 +0200

    added token and HMAC encoding to tv.aftonbladet url

--- lib/svtplay_dl/service/aftonbladet.py
+++ lib/svtplay_dl/service/aftonbladet.py
@@ -2,6 +2,7 @@
 # -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
 import json
 import re
+import logging
 
 from svtplay_dl.error import ServiceError
 from svtplay_dl.fetcher.hls import hlsparse
@@ -11,10 +12,10 @@ from svtplay_dl.utils.text import decode_html_entities
 
 class Aftonbladettv(Service):
     supported_domains = ["svd.se", "tv.aftonbladet.se"]
-
+    
     def get(self):
+        hdnea = self._login()
         data = self.get_urldata()
-
         match = re.search('data-player-config="([^"]+)"', data)
         if not match:
             match = re.search('data-svpPlayer-video="([^"]+)"', data)
@@ -24,7 +25,40 @@ class Aftonbladettv(Service):
                     yield ServiceError("Can't find video info")
                     return
         data = json.loads(decode_html_entities(match.group(1)))
-        yield from hlsparse(self.config, self.http.request("get", data["streamUrls"]["hls"]), data["streamUrls"]["hls"], output=self.output)
+        url = f"{data['streamUrls']['hls']}{hdnea}"
+        yield from hlsparse(
+            config=self.config, 
+            res=self.http.request("get", url), 
+            url=url, 
+            output=self.output
+        )
+    
+    def _login(self):   
+        service = 375826
+        if self.config.get("token") is None:
+            return None
+        
+        # Get token
+        if (t := self.http.request(
+            "get",
+            f"https://svp-token-api.aftonbladet.se/svp/token/{service}?access=plus",
+            headers={"x-sp-id":self.config.get("token")},
+            )
+        ).status_code != 200:
+            logging.info(f"Can't get token")
+            return None
+        
+        #hmac encrypt token
+        if (hdnea:= self.http.request(
+            "get",
+            f"https://svp.vg.no/svp/token/v1/?vendor=ab&assetId={service}&expires={t.json()['expiry']}&hmac={t.json()['value']}",
+            )
+        ).status_code != 200:
+            logging.info(f"Can't get hdnea encryption")
+            return None
+        
+        hdnea =f"?hdnea={hdnea.text.replace('/', '%2F').replace('=', '%3D').replace(',', '%2C')}" 
+        return hdnea 
 
 
 class Aftonbladet(Service):
