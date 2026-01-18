fcc60e9bd5575b7ab726a6503aa0e8602d187466
Johan Andersson <j@i19.se>
2026-01-18 14:01:37 +0100

    postprocess: add hvc1 codec when we output to mkv

--- lib/svtplay_dl/postprocess/__init__.py
+++ lib/svtplay_dl/postprocess/__init__.py
@@ -105,7 +105,7 @@ class postprocess:
                 arguments += ["-bsf:a", "aac_adtstoasc"]
         if videotrack and "dvh1" in _getcodec(streams, videotrack):
             if self.config.get("output_format") == "mkv":
-                logging.warning("HDR and mkv is not supported.")
+                arguments += ["-tag:v", "hvc1"]
             arguments += ["-strict", "unofficial"]
         cmd = [self.detect]
         if self.config.get("only_video") or (not self.config.get("only_audio") or (not self.stream.audio and self.config.get("only_audio"))):
