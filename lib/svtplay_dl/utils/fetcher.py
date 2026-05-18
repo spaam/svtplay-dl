59fa4348b5fdbf84774c7d53a44b83be0857cf61
Johan Andersson <j@i19.se>
2026-05-18 21:47:53 +0200

    m3u8.filter_files: they added more ads at the start

--- lib/svtplay_dl/utils/fetcher.py
+++ lib/svtplay_dl/utils/fetcher.py
@@ -3,7 +3,7 @@ from difflib import SequenceMatcher
 
 def filter_files(m3u8):
     files = []
-    good = m3u8.media_segment[1]["URI"]
+    good = m3u8.media_segment[3]["URI"]
     for segment in m3u8.media_segment:
         if SequenceMatcher(None, good, segment["URI"]).ratio() > 0.7:
             files.append(segment)
